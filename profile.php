2647b8f3165acb0c668db19ecc7daaa9a80cced1
GoSpace <104591072+BurgerIsReal01@users.noreply.github.com>
2023-02-16 07:14:46 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -29,7 +29,7 @@
       <nav class="navbar fixed-top bg-light shadow" style="padding-bottom: 7px; padding-top: 7px;">
         <div class="bb1 container">
           <a class="nav-link" href=""><i class="bi bi-discord"></i></a>
-          <a class="nav-link px-2 text-secondary" href="imgupload.php"><i class="bi bi-cloud-arrow-up-fill"></i></i></a>
+          <a class="nav-link px-2 text-secondary" href="imgupload.php"><i class="bi bi-cloud-arrow-up-fill"></i></a>
           <h1 style="color: gray; margin-top: 7px;" class="nav-link px-2 text-secondary"><a class="nav-link border-bottom" href="index.php">ArtCODE LITE</a></h1>
           <a class="nav-link px-2 text-secondary" href="profile.php"><i class="bi bi-person-circle"></i></a>
           <div class="dropdown">
