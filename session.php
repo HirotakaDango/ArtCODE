0964c25d221411a160b94b4c83819f730472648d
GoSpace <104591072+BurgerIsReal01@users.noreply.github.com>
2023-02-17 13:36:46 +0700

    Update session.php

--- session.php
+++ session.php
@@ -11,7 +11,7 @@
     <center style="margin-bottom: 60px; font-weight: 800; color: gray;">
       <nav class="navbar fixed-top bg-light shadow" style="padding-bottom: 7px; padding-top: 7px;">
         <div class="bb1 container">
-          <a class="nav-link" href=""><i class="bi bi-discord"></i></a>
+          <a class="nav-link" href="https://discord.gg/KhgFJV2N6J"><i class="bi bi-discord"></i></a>
           <a class="nav-link px-2 text-secondary" href="imgupload.php"><i class="bi bi-cloud-arrow-up-fill"></i></a>
           <h1 style="color: gray; margin-top: 7px;" class="nav-link px-2 text-secondary"><a class="nav-link border-bottom" href="index.php">ArtCODE LITE</a></h1>
           <a class="nav-link px-2 text-secondary" href="profile.php"><i class="bi bi-person-circle"></i></a>
@@ -27,39 +27,56 @@
         </div>
       </nav>
     </center>
-    <center><h1>User Login and Registration</h1></center>
     <br>
-    <center><h2>Login</h2></center>
-    <div class="modal-body p-4 pt-0">
-        <form class="" action="session_code.php" method="post">
-          <div class="form-floating mb-3">
-            <input name="username" type="email" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
-          </div>
-          <div class="form-floating mb-3">
-            <input name="password" type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-          </div>
-          <button name="login" class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Login</button>
-        </form>
-    </div>
+    <div class="container">
+      <div id="login-form">
+        <center><h2 class="mb-5">Login</h2></center>
+        <div class="modal-body p-4 pt-0">
+          <form class="" action="session_code.php" method="post">
+            <div class="form-floating mb-3">
+              <input name="username" type="email" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com">
+              <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating mb-3">
+              <input name="password" type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password">
+              <label for="floatingPassword">Password</label>
+            </div>
+            <button name="login" class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Login</button>
+            <p>Don't have an account? <a href="#" onclick="showRegisterForm()">Register</a></p>
+          </form>
+        </div>
+      </div>
 
-    <center><h2>Register</h2></center>
-    <div class="modal-body p-4 pt-0">
-        <form class="" action="session_code.php" method="post">
-          <div class="form-floating mb-3">
-            <input name="username" type="email" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
-          </div>
-          <div class="form-floating mb-3">
-            <input name="password" type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-          </div>
-          <button name="register" class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Register</button>
-        </form>
+      <div id="register-form" style="display:none;">
+        <center><h2 class="mb-5">Register</h2></center>
+        <div class="modal-body p-4 pt-0">
+          <form class="" action="session_code.php" method="post">
+            <div class="form-floating mb-3">
+              <input name="username" type="email" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com">
+              <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating mb-3">
+              <input name="password" type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password">
+              <label for="floatingPassword">Password</label>
+            </div>
+            <button name="register" class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Register</button>
+            <p>Already have an account? <a href="#" onclick="showLoginForm()">Login</a></p>
+          </form>
+        </div>
+      </div>
     </div>
-    </div>
+    <script>
+      function showLoginForm() {
+        document.getElementById('login-form').style.display = 'block';
+        document.getElementById('register-form').style.display = 'none';
+      }
+
+      function showRegisterForm() {
+        document.getElementById('login-form').style.display = 'none';
+        document.getElementById('register-form').style.display = 'block';
+      }
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
   </body>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 </html>
