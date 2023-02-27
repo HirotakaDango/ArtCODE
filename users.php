796714554abc86770070eac059b9e326b39d5f80
GoSpace <104591072+BurgerIsReal01@users.noreply.github.com>
2023-02-27 17:35:41 +0700

    Update users.php

--- users.php
+++ users.php
@@ -62,28 +62,56 @@
         </div>
       </nav>
     </center>  
-    <div class="container">
-      <h4 class="text-secondary text-center fw-bold">All Creators</h4>
+    <div>
+      <h4 class="text-secondary ms-2 fw-bold"><i class="bi bi-people-fill"></i> All Creators</h4>
+  
+      <!-- Add a search input field -->
+      <div class="input-group mb-3">
+        <input type="text" class="form-control me-2 ms-2" placeholder="Search creators" id="search-input">
+      </div>
+
       <?php
-      // Establish SQLite connection
-      $db = new SQLite3('database.sqlite');
-    
-      // Retrieve all users from the database
-      $users = $db->query('SELECT * FROM users');
+        // Establish SQLite connection
+        $db = new SQLite3('database.sqlite');
+
+        // Retrieve all users from the database
+        $users = $db->query('SELECT * FROM users');
       ?>
-    
       <!-- Display each user's id as a button in a button group -->
       <div class="tag-buttons" role="group" aria-label="User buttons">
         <?php while ($user = $users->fetchArray()): ?>
-          <button type="button" class="btn btn-secondary tag-button" onclick="location.href='artist.php?id=<?= $user['id'] ?>'"><?= $user['artist'] ?></button>
+          <button type="button" class="btn btn-secondary tag-button artist" onclick="location.href='artist.php?id=<?= $user['id'] ?>'"><?= $user['artist'] ?></button>
         <?php endwhile; ?>
       </div>
-    
+
       <?php
-      // Close the SQLite connection
-      $db->close();
+        // Close the SQLite connection
+        $db->close();
       ?>
     </div>
+    <script>
+      // Get the search input element
+      const searchInput = document.getElementById('search-input');
+
+      // Get all the artist buttons
+      const artistButtons = document.querySelectorAll('.artist');
+
+      // Add an event listener to the search input field
+      searchInput.addEventListener('input', () => {
+        const searchTerm = searchInput.value.toLowerCase();
+
+        // Filter the artist buttons based on the search term
+        artistButtons.forEach(button => {
+          const artistName = button.textContent.toLowerCase();
+
+          if (artistName.includes(searchTerm)) {
+          button.style.display = 'inline-block';
+          } else {
+            button.style.display = 'none';
+          }
+        });
+      });
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
   </body>
