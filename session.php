108945df7856816614f32cf2c251a32631e8e478
赤葦だんご <104591072+BurgerIsReal01@users.noreply.github.com>
2023-03-01 12:12:23 +0700

    Update session.php

--- session.php
+++ session.php
@@ -6,7 +6,7 @@
     <link rel="manifest" href="manifest.json">
     <link rel="icon" type="image/png" href="icon/favicon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
   </head>
   <body>
     <br>
