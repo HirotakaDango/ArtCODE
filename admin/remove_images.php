192556576eab29c5f2968be2f06db4f76d49824d
赤葦だんご <104591072+BurgerIsReal01@users.noreply.github.com>
2023-03-02 15:53:30 +0700

    Update remove_images.php

--- admin/remove_images.php
+++ admin/remove_images.php
@@ -27,12 +27,14 @@ if (isset($_POST['id'])) {
     header('Location: ' . $_SERVER['PHP_SELF']);
     exit;
 }
+
+// Query to get all images from the "images" table
+$stmt = $db->query('SELECT * FROM images ORDER BY id DESC');
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Images</title>
     <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
@@ -46,37 +48,73 @@ if (isset($_POST['id'])) {
       <li class="nav-item"><a class="nav-link" href="../admin/remove_all.php"><i class="bi bi-person-fill-exclamation"></i></a></li>
     </ul>
     <div class="container mt-5">
-      <div class="card">
-        <?php
-          // Query to get all images from the "images" table
-          $stmt = $db->query('SELECT * FROM images');
-
-          // Loop through the results and display images
-          while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-          // Display the image filename
-          echo '<div class="card-header bg-primary text-white fw-bold">
-                  <i class="bi bi-images"></i>
-                  Images
-                </div>
-                <div class="card-body">
-                  <img src="../thumbnails/' . $row['filename'] . '" class="rounded">
-                  <h5 class="card-title mt-1">' . $row['filename'] . '</h5>';
-
-                  // Display the image thumbnail
-                  echo '<p class="card-text fw-bold mt-1">Username: ' . $row['username'] . '</p>';
-                  echo '<p class="card-text fw-bold mt-1">Tags: ' . $row['tags'] . '</p>';
-            
-                  // Display a button to delete the image
-                  echo '<form method="post" action="" class="mt-3">
-                          <input type="hidden" name="id" value="' . $row['id'] . '">
-                          <button type="submit" class="btn btn-danger" onclick="return confirm(\'Are you sure you want to delete this image?\')">
+    <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?> 
+        <div class="card mb-2">
+            <div class="card-header bg-primary text-white fw-bold">
+                <i class="bi bi-images"></i>
+                Images
+            </div>
+            <div class="card-body">
+                    <img data-src="../thumbnails/<?= $row['filename'] ?>" class="rounded lazy-load" style="width: 100%; height: 200px;">
+                    <h5 class="card-title mt-1"><?= $row['filename'] ?></h5>
+                    <p class="card-text fw-bold mt-1">Username: <?= $row['username'] ?></p>
+                    <p class="card-text fw-bold mt-1">Tags: <?= $row['tags'] ?></p>
+                    <form method="post" action="" class="mt-3">
+                        <input type="hidden" name="id" value="<?= $row['id'] ?>">
+                        <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this image?')">
                             <i class="bi bi-trash"></i> Remove
-                          </button>
-                        </form>
-                      </div>
-                    </div>';
-        }?>
-      </div>
+                        </button>
+                    </form>
+            </div>
+        </div>
+      <?php endwhile; ?> 
     </div>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+          let lazyloadImages;
+          if("IntersectionObserver" in window) {
+            lazyloadImages = document.querySelectorAll(".lazy-load");
+            let imageObserver = new IntersectionObserver(function(entries, observer) {
+              entries.forEach(function(entry) {
+                if(entry.isIntersecting) {
+                  let image = entry.target;
+                  image.src = image.dataset.src;
+                  image.classList.remove("lazy-load");
+                  imageObserver.unobserve(image);
+                }
+              });
+            });
+            lazyloadImages.forEach(function(image) {
+              imageObserver.observe(image);
+            });
+          } else {
+            let lazyloadThrottleTimeout;
+            lazyloadImages = document.querySelectorAll(".lazy-load");
+
+            function lazyload() {
+              if(lazyloadThrottleTimeout) {
+                clearTimeout(lazyloadThrottleTimeout);
+              }
+              lazyloadThrottleTimeout = setTimeout(function() {
+                let scrollTop = window.pageYOffset;
+                lazyloadImages.forEach(function(img) {
+                  if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                    img.src = img.dataset.src;
+                    img.classList.remove('lazy-load');
+                  }
+                });
+                if(lazyloadImages.length == 0) {
+                  document.removeEventListener("scroll", lazyload);
+                  window.removeEventListener("resize", lazyload);
+                  window.removeEventListener("orientationChange", lazyload);
+                }
+              }, 20);
+            }
+            document.addEventListener("scroll", lazyload);
+            window.addEventListener("resize", lazyload);
+            window.addEventListener("orientationChange", lazyload);
+          }
+        })
+    </script>
 </body>
 </html>
