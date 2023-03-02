51dd93d40802c31e210f2bb29b7016a7eb4e2750
赤葦だんご <104591072+BurgerIsReal01@users.noreply.github.com>
2023-03-02 23:50:07 +0700

    Update session_code.php

--- session_code.php
+++ session_code.php
@@ -9,13 +9,13 @@ $db->exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMEN
 
 // Check if the user is logging in or registering
 if (isset($_POST['login'])) {
-  $username = htmlspecialchars($_POST['username']);
-  $password = htmlspecialchars($_POST['password']);
+  $username = substr(htmlspecialchars($_POST['username']), 0, 40);
+  $password = substr(htmlspecialchars($_POST['password']), 0, 40);
 
   // Check if the user exists in the database
   $stmt = $db->prepare("SELECT * FROM users WHERE username = :username AND password = :password");
-  $stmt->bindValue(':username', $username);
-  $stmt->bindValue(':password', $password);
+  $stmt->bindValue(':username', $username, SQLITE3_TEXT);
+  $stmt->bindValue(':password', $password, SQLITE3_TEXT);
   $result = $stmt->execute();
   $user = $result->fetchArray();
   if ($user) {
@@ -36,13 +36,13 @@ if (isset($_POST['login'])) {
     echo "Incorrect username or password.";
   }
 } elseif (isset($_POST['register'])) {
-  $username = htmlspecialchars($_POST['username']);
-  $password = htmlspecialchars($_POST['password']);
-  $artist = htmlspecialchars($_POST['artist']);
+  $username = substr(htmlspecialchars($_POST['username']), 0, 40);
+  $password = substr(htmlspecialchars($_POST['password']), 0, 40);
+  $artist = substr(htmlspecialchars($_POST['artist']), 0, 40);
 
   // Check if the username is already taken
   $stmt = $db->prepare("SELECT * FROM users WHERE username = :username");
-  $stmt->bindValue(':username', $username);
+  $stmt->bindValue(':username', $username, SQLITE3_TEXT);
   $result = $stmt->execute();
   $user = $result->fetchArray();
   if ($user) {
@@ -50,9 +50,9 @@ if (isset($_POST['login'])) {
   } else {
     // Add the new user to the database
     $stmt = $db->prepare("INSERT INTO users (username, password, artist) VALUES (:username, :password, :artist)");
-    $stmt->bindValue(':username', $username);
-    $stmt->bindValue(':password', $password);
-    $stmt->bindValue(':artist', $artist);
+    $stmt->bindValue(':username', $username, SQLITE3_TEXT);
+    $stmt->bindValue(':password', $password, SQLITE3_TEXT);
+    $stmt->bindValue(':artist', $artist, SQLITE3_TEXT);
     $stmt->execute();
     
     // Generate a unique session ID and store it in a cookie
