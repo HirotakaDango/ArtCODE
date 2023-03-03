5d8165d4226fea4630ed0819dbba1c53a0208536
赤葦だんご <104591072+BurgerIsReal01@users.noreply.github.com>
2023-03-04 02:52:49 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -15,6 +15,7 @@ if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   </head>
   <body>
    <ul class="me-2 ms-2 mt-2 nav nav-pills nav-fill justify-content-center">
@@ -49,31 +50,79 @@ if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
           </div>
         </div>
         <div class="col-md-6 mt-2">
-          <div class="card">
-            <div class="card-header bg-primary text-white fw-bold">
-              <i class="bi bi-images"></i>
-              Images with Tags
+            <div class="card">
+                <div class="card-header bg-primary text-white fw-bold">
+                    <i class="bi bi-images"></i>
+                    Images with Tags
+                </div>
+                <div class="card-body">
+                    <?php
+                    // Open the database file
+                    $db = new SQLite3('../database.sqlite');
+
+                    // Count the number of images in the "images" table
+                    $image_count = $db->querySingle('SELECT COUNT(*) FROM images');
+
+                    // Count the total number of tags in the "tags" column of the "images" table
+                    $tag_count = $db->querySingle('SELECT SUM(length(tags) - length(replace(tags, ",", "")) + 1) AS tag_count FROM images');
+
+                    // Close the database file
+                    $db->close();
+
+                    // Calculate the total size of all images
+                    $dir = "../images"; // set your directory path here
+                    $size = 0; // initialize the size variable to 0
+                    $image_count_data = 0; // initialize the image count variable to 0
+
+                    if ($handle = opendir($dir)) {
+                        while (($file = readdir($handle)) !== false) {
+                            if (in_array(strtolower(pathinfo($file, PATHINFO_EXTENSION)), array('jpg', 'jpeg', 'png', 'gif'))) {
+                                $image_count_data++; // increment the image count
+                                $size += filesize("$dir/$file"); // add the size of the image to the total size
+                            }
+                        }
+                        closedir($handle);
+                    }
+
+                    // Convert the total size to MB and store the values in a variable
+                    function convert_bytes_to_mb($bytes) {
+                        return number_format($bytes / 1048576, 2);
+                    }
+                    $total_size_data = convert_bytes_to_mb($size);
+
+                    // Display the image count, tag count, and total size of images as a line chart
+                    echo '<canvas id="myChart"></canvas>';
+                    echo "<p class='fw-bold text-secondary mt-3'>Total Images: " . $image_count_data . "</p>";
+                    echo "<p class='fw-bold text-secondary'>Total Tags: " . $tag_count . "</p>";
+                    echo "<p class='fw-bold text-secondary'>Total Size: " . $total_size_data . " MB</p>";
+                    ?>
+                    <script>
+                        var ctx = document.getElementById('myChart').getContext('2d');
+                        var myChart = new Chart(ctx, {
+                            type: 'doughnut',
+                            data: {
+                                labels: ['(' + <?php echo $image_count_data; ?> + ' images)', '(' + <?php echo $tag_count; ?> + ' tags)', '(' + <?php echo $total_size_data; ?> + ' MB)'],
+                                datasets: [{
+                                    label: 'Image Data',
+                                    data: [<?php echo $image_count_data; ?>, <?php echo $tag_count; ?>, <?php echo $total_size_data; ?>],
+                                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                                    borderColor: 'rgba(255, 99, 132, 1)',
+                                    borderWidth: 1
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true
+                                        }
+                                    }]
+                                }
+                            }
+                        });
+                    </script> 
+                </div>
             </div>
-            <div class="card-body">
-              <?php
-                // Open the database file
-                $db = new SQLite3('../database.sqlite');
-
-                // Count the number of images in the "images" table
-                $image_count = $db->querySingle('SELECT COUNT(*) FROM images');
-
-                // Count the total number of tags in the "tags" column of the "images" table
-                $tag_count = $db->querySingle('SELECT SUM(length(tags) - length(replace(tags, ",", "")) + 1) AS tag_count FROM images');
-
-                // Output the image count and tag count in the card body
-                echo "<h1 class='fw-bold text-secondary'>" . $image_count . "</h1>";
-                echo "<p class='fw-bold text-secondary'>Total Tags: " . $tag_count . "</p>";
-
-                // Close the database file
-                $db->close();
-              ?>
-            </div>
-          </div>
         </div>
       </div>
     </div>
