449546edab7ee14feba38fa4f88ed7664d6a58d8
赤葦だんご <104591072+BurgerIsReal01@users.noreply.github.com>
2023-03-05 00:36:14 +0700

    Update edit_image.php

--- edit_image.php
+++ edit_image.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if (!isset($_SESSION['username'])) {
+  header("Location: session.php");
+  exit;
+}
+
 // Connect to SQLite database
 $db = new PDO('sqlite:database.sqlite');
 
