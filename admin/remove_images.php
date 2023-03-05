afae82faa9adaa3a2ecae9c4668b95fa5db74bb8
赤葦だんご <104591072+BurgerIsReal01@users.noreply.github.com>
2023-03-05 19:15:56 +0700

    Update remove_images.php

--- admin/remove_images.php
+++ admin/remove_images.php
@@ -41,7 +41,7 @@ $stmt = $db->query('SELECT * FROM images ORDER BY id DESC');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
-   <ul class="me-2 ms-2 mt-2 nav nav-pills nav-fill justify-content-center">
+    <ul class="me-2 ms-2 mt-2 nav nav-pills nav-fill justify-content-center">
       <li class="nav-item"><a class="nav-link" href="../admin/index.php"><i class="bi bi-house-fill"></i></a></li>
       <li class="nav-item"><a class="nav-link" aria-current="page" href="../admin/edit_users.php"><i class="bi bi-person-fill-gear"></i></a></li>
       <li class="nav-item"><a class="nav-link active" href="../admin/remove_images.php"><i class="bi bi-images"></i></a></li> 
