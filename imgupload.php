8553cdd756cd3093def7d0296a83d85ee341c695
赤葦だんご <104591072+BurgerIsReal01@users.noreply.github.com>
2023-03-10 15:18:00 +0700

    Add files via upload

---
+++ imgupload.php
@@ -0,0 +1,225 @@
+<?php
+  session_start();
+  if (!isset($_SESSION['username'])) {
+    header("Location: session.php");
+    exit;
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ArtCODE</title>
+    <link rel="manifest" href="manifest.json">
+    <link rel="icon" type="image/png" href="icon/favicon.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+      <div class="container-fluid">
+        <button class="navbar-toggler1 d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+          <img src="icon/toggle1.svg" width="22" height="22">
+        </button> 
+        <a class="navbar-brand text-secondary fw-bold" href="index.php">
+          ArtCODE
+        </a>
+          <div class="dropdown nav-right ">
+            <a class="nav-link px-2 text-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="bi bi-person-circle fs-5"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
+              <li><a class="dropdown-item text-secondary fw-bold" href="profile.php"><i class="bi bi-person-circle"></i> Profile</a></li>
+              <li><a class="dropdown-item text-secondary fw-bold" href="setting.php"><i class="bi bi-gear-fill"></i> Settings</a></li>
+              <li><a class="dropdown-item text-secondary fw-bold" href="favorite.php"><i class="bi bi-heart-fill"></i> Favorites</a></li>
+              <li><a class="dropdown-item text-secondary fw-bold" href="logout.php"><i class="bi bi-door-open-fill"></i> Logout</a></li>
+            </ul>
+          </div> 
+        <div class="offcanvas offcanvas-start w-50" tabindex="-1" id="navbar" aria-labelledby="navbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="navbarLabel">Menu</h5>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="index.php">
+                  <i class="bi bi-house-fill fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Home</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="forum-chat/index.php">
+                  <i class="bi bi-chat-left-dots-fill fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Forum</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="imgupload.php">
+                  <i class="bi bi-cloud-arrow-up-fill fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Uploads</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="popular.php">
+                  <i class="bi bi-graph-up fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Popular</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="tags.php">
+                  <i class="bi bi-tags-fill fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Tags</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="users.php">
+                  <i class="bi bi-people-fill fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Users</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="global.php">
+                  <i class="bi bi-clock-history fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Recents</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <section class="gallery-links mt-2">
+      <center>
+        <div class="container">
+          <h2 class="mt-3 mb-3" style="font-family: sans-serif; float: center; color: gray; font-weight: 800;">UPLOAD IMAGE</h2>
+          <img class="image" id="file-ip-1-preview" style="height: 350px; border-radius: 8px; width: 100%; margin-bottom: 15px;">
+          <div class="gallery-upload">
+            <form action="upload.php" method="post" enctype="multipart/form-data">
+              <input class="form-control" type="file" name="image" type="file" id="file-ip-1" accept="image/*" onchange="showPreview(event);">
+              <input class="form-control mt-3" type="text" name="title" placeholder="Enter title for your image" maxlength="40"> 
+              <textarea class="form-control mt-3" type="text" name="imgdesc" placeholder="Enter description for your image" maxlength="200"></textarea>
+              <input class="form-control mt-3" type="text" name="tags" placeholder="Enter tags separated by comma">
+              <input class="form-control mt-3" type="text" name="link" placeholder="Enter link for your image" maxlength="120"> 
+              <input class="btn btn-primary fw-bold w-100 mt-3" type="submit" name="submit" value="upload">
+            </form>
+          </div>
+        </div>
+      </center>
+    </section>
+    <div class="mt-5"></div>
+    <style>
+      @media (min-width: 768px) {
+        .navbar-nav {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+      
+        .nav-center {
+          margin-left: 30px;
+        }
+      
+        .nav-right {
+          position: absolute;
+          right: 10px;
+          top: 10;
+          align-items: center;
+        }
+      }
+      
+      @media (max-width: 767px) {
+        .navbar-brand {
+          position: static;
+          display: block;
+          text-align: center;
+          margin: auto;
+          transform: none;
+        }
+
+        .navbar-brand {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          font-size: 18px;
+        }
+      }
+    
+      .navbar {
+        height: 45px;
+      }
+      
+      .navbar-brand {
+        font-size: 18px;
+      }
+
+      @media (min-width: 992px) {
+        .navbar-toggler1 {
+          display: none;
+        }
+      }
+    
+      .navbar-toggler1 {
+        background-color: #ededed;
+        border: none;
+        font-size: 8px;
+        margin-top: -2px;
+        border-radius: 5px;
+        padding: 6px;
+        transition: background-color 0.3s ease; 
+      }
+
+      .navbar-toggler1:hover {
+        background-color: rgba(0,0,0,0.2);
+      }
+
+      .image {
+        text-align: center;
+        object-fit: cover;
+        margin: auto;
+        border: 4px solid #e6e5e3;
+      }
+    </style>
+    <script>
+      const toggleSwitch = document.querySelector('#dark-mode-toggle');
+      const body = document.querySelector('body');
+      
+      // Set initial state of toggle based on user preference
+      if (localStorage.getItem('dark-mode') === 'enabled') {
+        toggleSwitch.checked = true;
+        body.classList.add('dark-mode');
+      }
+    
+      // Listen for toggle change events
+      toggleSwitch.addEventListener('change', () => {
+        if (toggleSwitch.checked) {
+          localStorage.setItem('dark-mode', 'enabled');
+          body.classList.add('dark-mode');
+        } else {
+          localStorage.setItem('dark-mode', null);
+          body.classList.remove('dark-mode');
+        }
+      });
+    </script>
+    <script>
+      function showPreview(event){
+        if(event.target.files.length > 0){
+          var src = URL.createObjectURL(event.target.files[0]);
+          var preview = document.getElementById("file-ip-1-preview");
+          preview.src = src;
+          preview.style.display = "block";
+        }
+      }
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+  </body>
+</html>
