85d1cf1adcee37891fe32239b0bcfdef8877c209
赤葦だんご <104591072+BurgerIsReal01@users.noreply.github.com>
2023-03-14 23:49:48 +0700

    Add files via upload

---
+++ artist.php
@@ -0,0 +1,449 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+  header("Location: session.php");
+  exit;
+}
+
+$username = $_SESSION['username'];
+
+// Connect to SQLite database
+$db = new PDO('sqlite:database.sqlite');
+
+// Get the ID of the selected user from the URL
+$id = $_GET['id'];
+$query = $db->prepare('SELECT artist, `desc`, `bgpic`, pic, username FROM users WHERE id = :id');
+$query->bindParam(':id', $id);
+$query->execute();
+$user = $query->fetch(PDO::FETCH_ASSOC);
+$artist = $user['artist'];
+$desc = $user['desc'];
+$pic = $user['pic'];
+$bgpic = $user['bgpic'];
+
+// Get all images for the selected user from the images table
+$query = $db->prepare('SELECT images.id, images.filename FROM images JOIN users ON images.username = users.username WHERE users.id = :id ORDER BY images.id DESC');
+$query->bindParam(':id', $id);
+$query->execute();
+$images = $query->fetchAll(PDO::FETCH_ASSOC);
+
+// Check if the logged-in user is already following the selected user
+$query = $db->prepare('SELECT COUNT(*) FROM following WHERE follower_username = :follower_username AND following_username = :following_username');
+$query->bindParam(':follower_username', $username);
+$query->bindParam(':following_username', $user['username']);
+$query->execute();
+$is_following = $query->fetchColumn();
+
+// Get the number of followers for the selected user
+$query = $db->prepare('SELECT COUNT(*) FROM following WHERE following_username = :following_username');
+$query->bindParam(':following_username', $user['username']);
+$query->execute();
+$num_followers = $query->fetchColumn();
+
+// Get the number of people the selected user is following
+$query = $db->prepare('SELECT COUNT(*) FROM following WHERE follower_username = :follower_username');
+$query->bindParam(':follower_username', $user['username']);
+$query->execute();
+$num_following = $query->fetchColumn();
+
+// Handle following/unfollowing actions
+if (isset($_POST['follow'])) {
+  // Add a following relationship between the logged-in user and the selected user
+  $query = $db->prepare('INSERT INTO following (follower_username, following_username) VALUES (:follower_username, :following_username)');
+  $query->bindParam(':follower_username', $username);
+  $query->bindParam(':following_username', $user['username']);
+  $query->execute();
+  $is_following = true;
+  header("Location: artist.php?id={$id}");
+  exit;
+} elseif (isset($_POST['unfollow'])) {
+  // Remove the following relationship between the logged-in user and the selected user
+  $query = $db->prepare('DELETE FROM following WHERE follower_username = :follower_username AND following_username = :following_username');
+  $query->bindParam(':follower_username', $username);
+  $query->bindParam(':following_username', $user['username']);
+  $query->execute();
+  $is_following = false;
+  header("Location: artist.php?id={$id}");
+  exit;
+} 
+
+// Process any favorite/unfavorite requests
+if (isset($_POST['favorite'])) {
+  $image_id = $_POST['image_id'];
+
+  // Check if the image has already been favorited by the current user
+  $existing_fav = $db->query("SELECT COUNT(*) FROM favorites WHERE username = '{$_SESSION['username']}' AND image_id = $image_id")->fetchColumn();
+
+  if ($existing_fav == 0) {
+    $db->exec("INSERT INTO favorites (username, image_id) VALUES ('{$_SESSION['username']}', $image_id)");
+  }
+
+  // Redirect to the same page to prevent duplicate form submissions
+  header("Location: artist.php?id={$id}");
+  exit();
+
+} elseif (isset($_POST['unfavorite'])) {
+  $image_id = $_POST['image_id'];
+  $db->exec("DELETE FROM favorites WHERE username = '{$_SESSION['username']}' AND image_id = $image_id");
+
+  // Redirect to the same page to prevent duplicate form submissions
+  header("Location: artist.php?id={$id}");
+  exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $artist; ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar fixed-top navbar-expand-md navbar-light bg-white shadow-sm">
+      <div class="container-fluid">
+        <button class="navbar-toggler1 d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+          <img src="icon/toggle1.svg" width="22" height="22">
+        </button> 
+        <a class="navbar-brand text-secondary fw-bold" href="index.php">
+          ArtCODE
+        </a>
+          <div class="dropdown nav-right">
+            <a class="nav-link px-2 text-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="bi bi-person-circle fs-5"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
+              <li><a class="dropdown-item text-secondary fw-bold" href="profile.php"><i class="bi bi-person-circle"></i> Profile</a></li>
+              <li><a class="dropdown-item text-secondary fw-bold" href="setting.php"><i class="bi bi-gear-fill"></i> Settings</a></li>
+              <li><a class="dropdown-item text-secondary fw-bold" href="favorite.php"><i class="bi bi-heart-fill"></i> Favorites</a></li>
+              <li><a class="dropdown-item text-secondary fw-bold" href="logout.php"><i class="bi bi-door-open-fill"></i> Logout</a></li>
+            </ul>
+          </div> 
+        <div class="offcanvas offcanvas-start w-50" tabindex="-1" id="navbar" aria-labelledby="navbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title text-secondary" id="navbarLabel">Menu</h5>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fw-bold">
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="index.php">
+                  <i class="bi bi-house-fill fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Home</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="forum-chat/index.php">
+                  <i class="bi bi-chat-left-dots-fill fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Forum</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="imgupload.php">
+                  <i class="bi bi-cloud-arrow-up-fill fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Uploads</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="popular.php">
+                  <i class="bi bi-star-fill fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Popular</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="tags.php">
+                  <i class="bi bi-tags-fill fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Tags</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="users.php">
+                  <i class="bi bi-people-fill fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Users</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="global.php">
+                  <i class="bi bi-clock-history fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Recents</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="news.php">
+                  <i class="bi bi-newspaper fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Update & News</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-center" href="support.php">
+                  <i class="bi bi-headset fs-5"></i>
+                  <span class="d-md-none d-lg-inline ms-2">Support</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="roow mb-2">
+      <div class="cool-6 text-center">
+        <div class="caard art">
+          <div class="col-md-5 order-md-1 mt-3 b-radius" style="background-image: url('<?php echo $bgpic; ?>'); background-size: cover; height: 200px; width: 100%;">
+            <img class="img-thumbnail rounded-circle" src="<?php echo $pic; ?>" alt="Profile Picture" style="width: 110px; height: 110px; border-radius: 4px; margin-top: 45px;">
+          </div> 
+        </div>
+      </div>
+      <div class="cool-6 border-down">
+        <div class="caard art text-center">
+          <h3 class="text-secondary mt-2 fw-bold"><?php echo $artist; ?></h3>
+          <form method="post">
+            <?php if ($is_following): ?>
+              <button class="btn btn-sm btn-danger rounded-pill fw-bold" type="submit" name="unfollow"><i class="bi bi-person-dash-fill"></i> unfollow</button>
+            <?php else: ?>
+              <button class="btn btn-sm btn-primary rounded-pill fw-bold" type="submit" name="follow"><i class="bi bi-person-fill-add"></i> follow</button>
+            <?php endif; ?>
+          </form>
+          <p class="text-secondary ms-1 mt-2 fw-bold">
+            <a class="text-secondary" href="follower.php?id=<?php echo $id; ?>"><?php echo $num_followers ?> followers</a><span style="padding-right: 10px;"></span>
+            <a class="text-secondary" href="following.php?id=<?php echo $id; ?>"><?php echo $num_following ?> following</a><span style="padding-right: 10px;"></span> 
+            <?php echo count($images); ?> posts
+          </p>
+          <p class="text-secondary text-center fw-bold"><?php echo $desc; ?></p>
+        </div>
+      </div>
+    </div>
+    <div class="images">
+      <?php foreach ($images as $image): ?> 
+        <div class="image-container">
+          <a href="image.php?filename=<?php echo $image['filename']; ?>">
+            <img class="lazy-load" data-src="thumbnails/<?php echo $image['filename']; ?>">
+          </a>
+          <div class="favorite-btn">
+            <?php
+              $is_favorited = $db->query("SELECT COUNT(*) FROM favorites WHERE username = '{$_SESSION['username']}' AND image_id = {$image['id']}")->fetchColumn();
+              if ($is_favorited) {
+            ?>
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) . '?id=' . urlencode($id); ?>" method="POST">
+              <input type="hidden" name="image_id" value="<?php echo $image['id']; ?>">
+              <button style="margin-top: -74px; margin-left: 8px; font-size: 10px;" type="submit" class="btn btn-danger rounded-5 fw-bold" name="unfavorite"><i class="bi bi-heart-fill"></i></button>
+            </form>
+            <?php } else { ?>
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) . '?id=' . urlencode($id); ?>" method="POST">
+              <input type="hidden" name="image_id" value="<?php echo $image['id']; ?>">
+              <button style="margin-top: -74px; margin-left: 8px; font-size: 10px;" type="submit" class="btn btn-danger rounded-5 fw-bold" name="favorite"><i class="bi bi-heart"></i></button>
+            </form>
+            <?php } ?>
+          </div> 
+        </div>
+      <?php endforeach; ?> 
+    </div>
+    <div class="mt-5"></div>
+    
+    <style>
+      @media (min-width: 768px) {
+        .navbar-nav {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+      
+        .nav-center {
+          margin-left: 15px;
+          margin-right: 15px;
+        }
+      
+        .nav-right {
+          position: absolute;
+          right: 10px;
+          top: 10;
+          align-items: center;
+        }
+      }
+      
+      @media (max-width: 767px) {
+        .navbar-brand {
+          position: static;
+          display: block;
+          text-align: center;
+          margin: auto;
+          transform: none;
+        }
+
+        .navbar-brand {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          font-size: 18px;
+        }
+      }
+    
+      .navbar {
+        height: 45px;
+      }
+      
+      .navbar-brand {
+        font-size: 18px;
+      }
+
+      @media (min-width: 992px) {
+        .navbar-toggler1 {
+          display: none;
+        }
+      }
+    
+      .navbar-toggler1 {
+        background-color: #ededed;
+        border: none;
+        font-size: 8px;
+        margin-top: -2px;
+        margin-left: 8px;
+        border-radius: 5px;
+        padding: 6px;
+        transition: background-color 0.3s ease; 
+      }
+
+      .navbar-toggler1:hover {
+        background-color: rgba(0,0,0,0.2);
+      }
+
+      .image-container {
+        margin-bottom: -24px;  
+      }
+      
+      .images {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+        grid-gap: 2px;
+        justify-content: center;
+        margin-right: 3px;
+        margin-left: 3px;
+      }
+
+      .images a {
+        display: block;
+        border-radius: 4px;
+        overflow: hidden;
+        border: 2px solid #ccc;
+      }
+
+      .images img {
+        width: 100%;
+        height: auto;
+        object-fit: cover;
+        height: 200px;
+        transition: transform 0.5s ease-in-out;
+      }
+    
+      .roow {
+        display: flex;
+        flex-wrap: wrap;
+        border-radius: 5px;
+        border: 2px solid lightgray;
+        margin-right: 10px;
+        margin-left: 10px;
+        margin-top: 55px;
+      }
+
+      .cool-6 {
+        width: 50%;
+        padding: 0 15px;
+      }
+
+      .caard {
+        background-color: #fff;
+        margin-bottom: 15px;
+      }
+      
+      .b-radius {
+        border-radius: 10px;
+      }
+
+      .art {
+        border-radius: 10px;
+      }
+
+      @media (max-width: 768px) {
+        .roow {
+          border: none;
+          margin-right: 0;
+          margin-left: 0;
+          margin-top: 32px;
+        }
+        
+        .cool-6 {
+          width: 100%;
+          padding: 0;
+        }
+        
+        .b-radius {
+          border-right: none;
+          border-left: none;
+          border-top: 1px solid lightgray;
+          border-bottom: 1px solid lightgray; 
+          border-radius: 0;
+        }
+        
+        .border-down {
+          border-bottom: 2px solid lightgray;
+        }
+      }
+    </style>
+    <script>
+      document.addEventListener("DOMContentLoaded", function() {
+        let lazyloadImages;
+        if("IntersectionObserver" in window) {
+          lazyloadImages = document.querySelectorAll(".lazy-load");
+          let imageObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+              if(entry.isIntersecting) {
+                let image = entry.target;
+                image.src = image.dataset.src;
+                image.classList.remove("lazy-load");
+                imageObserver.unobserve(image);
+              }
+            });
+          });
+          lazyloadImages.forEach(function(image) {
+            imageObserver.observe(image);
+          });
+        } else {
+          let lazyloadThrottleTimeout;
+          lazyloadImages = document.querySelectorAll(".lazy-load");
+
+          function lazyload() {
+            if(lazyloadThrottleTimeout) {
+              clearTimeout(lazyloadThrottleTimeout);
+            }
+            lazyloadThrottleTimeout = setTimeout(function() {
+              let scrollTop = window.pageYOffset;
+              lazyloadImages.forEach(function(img) {
+                if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                  img.src = img.dataset.src;
+                  img.classList.remove('lazy-load');
+                }
+              });
+              if(lazyloadImages.length == 0) {
+                document.removeEventListener("scroll", lazyload);
+                window.removeEventListener("resize", lazyload);
+                window.removeEventListener("orientationChange", lazyload);
+              }
+            }, 20);
+          }
+          document.addEventListener("scroll", lazyload);
+          window.addEventListener("resize", lazyload);
+          window.addEventListener("orientationChange", lazyload);
+        }
+      })
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+  </body>
+</html>
