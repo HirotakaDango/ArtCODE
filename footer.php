3a222c2c3f43e93c4f4e92120058b7ef69ec8c9d
赤葦だんご <104591072+BurgerIsReal01@users.noreply.github.com>
2023-03-18 11:53:32 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+  <div class="container">
+    <footer class="py-3 my-4">
+      <ul class="nav justify-content-center pb-3 mb-3">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted"><i class="bi bi-twitter"></i> Twitter</a></li>
+        <li class="nav-item"><a href="https://github.com/BurgerIsReal01/ArtCODE" class="nav-link px-2 text-muted"><i class="bi bi-github"></i> Github</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted"><i class="bi bi-facebook"></i> Facebook</a></li>
+      </ul>
+      <p class="text-center text-muted">© 2022 - 2023, ArtCODE</p>
+    </footer>
+  </div> 
