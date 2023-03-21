a37898885a250e34228c06c5c3d14b7e333ae544
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-03-21 19:25:45 +0700

    Add files via upload

---
+++ index_desc.php
@@ -0,0 +1,172 @@
+<?php
+// Set the limit of images per page
+$limit = 100;
+
+// Get the current page number, default to 1
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+
+// Calculate the offset based on the current page number and limit
+$offset = ($page - 1) * $limit;
+
+// Get the total number of images
+$total = $db->querySingle("SELECT COUNT(*) FROM images");
+
+// Get the images for the current page
+$stmt = $db->prepare("SELECT * FROM images ORDER BY id DESC LIMIT ?, ?");
+$stmt->bindValue(1, $offset, SQLITE3_INTEGER);
+$stmt->bindValue(2, $limit, SQLITE3_INTEGER);
+$result = $stmt->execute();
+?>
+
+    <div class="dropdown">
+      <button class="btn btn-sm fw-bold rounded-pill ms-2 btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="bi bi-images"></i> sort by
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item fw-bold active" href="?by=newest&page=<?php echo isset($_GET['page']) ? $_GET['page'] : '1'; ?>">Newest</a></li>
+        <li><a class="dropdown-item fw-bold" href="?by=oldest&page=<?php echo isset($_GET['page']) ? $_GET['page'] : '1'; ?>">Oldest</a></li>
+      </ul> 
+    </div> 
+    <div class="images mb-2 mt-2">
+      <?php while ($image = $result->fetchArray()): ?>
+        <div class="image-container">
+          <a href="image.php?filename=<?php echo $image['filename']; ?>">
+            <img class="lazy-load" data-src="thumbnails/<?php echo $image['filename']; ?>">
+          </a>
+          <div class="favorite-btn">
+            <?php
+              $is_favorited = $db->querySingle("SELECT COUNT(*) FROM favorites WHERE username = '$username' AND image_id = {$image['id']}");
+              if ($is_favorited) {
+            ?>
+              <form method="POST">
+                <input type="hidden" name="image_id" value="<?php echo $image['id']; ?>">
+                <button style="margin-top: -68px;" type="submit" class="p-b3 btn btn-sm rounded btn-dark opacity-50 fw-bold" name="unfavorite"><i class="bi bi-heart-fill"></i></button>
+              </form>
+            <?php } else { ?>
+              <form method="POST">
+                <input type="hidden" name="image_id" value="<?php echo $image['id']; ?>">
+                <button style="margin-top: -68px;" type="submit" class="p-b3 btn btn-sm rounded btn-dark opacity-50 fw-bold" name="favorite"><i class="bi bi-heart"></i></button>
+              </form>
+            <?php } ?>  
+          </div> 
+        </div>
+      <?php endwhile; ?>
+    </div>
+    <div class="mt-5 mb-2 d-flex justify-content-center btn-toolbar container">
+      <?php
+        $totalPages = ceil($total / $limit);
+        $prevPage = $page - 1;
+        $nextPage = $page + 1;
+
+        if ($page > 1) {
+          echo '<a type="button" class="btn rounded-pill fw-bold btn-secondary opacity-50 me-1" href="?by=newest&page=' . $prevPage . '"><i class="bi bi-arrow-left-circle-fill"></i> prev</a>';
+        }
+        if ($page < $totalPages) {
+          echo '<a type="button" class="btn rounded-pill fw-bold btn-secondary opacity-50 ms-1" href="?by=newest&page=' . $nextPage . '">next <i class="bi bi-arrow-right-circle-fill"></i></a>';
+        }
+      ?>
+    </div>
+    <style>
+      @media (min-width: 769px) {
+        
+        .p-b3 {
+          margin-left: 6px;
+          border-radius: 4px;
+        } 
+      }
+      
+      @media (max-width: 767px) {
+        
+        .p-b3 {
+          margin-left: 5px;
+          border-radius: 4px;
+        }
+      } 
+      
+      .image-container {
+        margin-bottom: -24px;  
+      }
+      
+      .images {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+        grid-gap: 2px;
+        justify-content: center;
+        margin-right: 3px;
+        margin-left: 3px;
+      }
+
+      .images a {
+        display: block;
+        border-radius: 4px;
+        overflow: hidden;
+        border: 2px solid #ccc;
+      }
+
+      .images img {
+        width: 100%;
+        height: auto;
+        object-fit: cover;
+        height: 200px;
+        transition: transform 0.5s ease-in-out;
+      }
+    
+    </style>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+          let lazyloadImages;
+          if("IntersectionObserver" in window) {
+            lazyloadImages = document.querySelectorAll(".lazy-load");
+            let imageObserver = new IntersectionObserver(function(entries, observer) {
+              entries.forEach(function(entry) {
+                if(entry.isIntersecting) {
+                  let image = entry.target;
+                  image.src = image.dataset.src;
+                  image.classList.remove("lazy-load");
+                  imageObserver.unobserve(image);
+                }
+              });
+            });
+            lazyloadImages.forEach(function(image) {
+              imageObserver.observe(image);
+            });
+          } else {
+            let lazyloadThrottleTimeout;
+            lazyloadImages = document.querySelectorAll(".lazy-load");
+
+            function lazyload() {
+              if(lazyloadThrottleTimeout) {
+                clearTimeout(lazyloadThrottleTimeout);
+              }
+              lazyloadThrottleTimeout = setTimeout(function() {
+                let scrollTop = window.pageYOffset;
+                lazyloadImages.forEach(function(img) {
+                  if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                    img.src = img.dataset.src;
+                    img.classList.remove('lazy-load');
+                  }
+                });
+                if(lazyloadImages.length == 0) {
+                  document.removeEventListener("scroll", lazyload);
+                  window.removeEventListener("resize", lazyload);
+                  window.removeEventListener("orientationChange", lazyload);
+                }
+              }, 20);
+            }
+            document.addEventListener("scroll", lazyload);
+            window.addEventListener("resize", lazyload);
+            window.addEventListener("orientationChange", lazyload);
+          }
+        })
+    </script>
+    <script>
+        if ('serviceWorker' in navigator) {
+          window.addEventListener('load', function() {
+            navigator.serviceWorker.register('sw.js').then(function(registration) {
+              console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function(err) {
+              console.log('ServiceWorker registration failed: ', err);
+            });
+          });
+        }
+    </script>
