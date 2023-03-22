6078a3b49a669bd3f39d0ba2ccddf1997a8da1c8
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-03-23 04:24:53 +0700

    Add files via upload

---
+++ help.php
@@ -0,0 +1,48 @@
+<div class="modal fade" id="help" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered modal-fullscreen">
+    <div class="modal-content">
+      <div class="modal-header border-bottom-0">
+        <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Support</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body text-secondary fw-bold">
+        <h5 class="ms-2 mt-2 text-secondary fw-bold"><i class="bi bi-headset"></i> Support & Help Center</h5>
+        <div class="container-fluid">
+          <div class="row row-cols-1 row-cols-md-3 g-4 fw-bold">
+            <div class="col">
+              <div class="card h-100">
+                <i class="bi bi-telephone-fill text-secondary text-center" style="font-size: 100px;"></i>
+                <div class="card-body">
+                  <h5 class="card-title fw-bold">Contact Us</h5>
+                  <p class="card-text text-secondary">If you have any problem or issue, contact us.</p>
+                  <a class="btn btn-primary rounded-pill fw-bold" href="tel:">Contact Us</a>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <i class="bi bi-envelope-at-fill text-secondary text-center" style="font-size: 100px;"></i>
+                <div class="card-body">
+                  <h5 class="card-title fw-bold">Email Us</h5>
+                  <p class="card-text text-secondary">If you have any problem or issue, send us a mail.</p>
+                  <a class="btn btn-primary rounded-pill fw-bold" href="mailto:" class="btn btn-primary">Email Us</a>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <i class="bi bi-chat-left-text-fill text-secondary text-center" style="font-size: 100px;"></i>
+                <div class="card-body">
+                  <h5 class="card-title fw-bold">Message Us</h5>
+                  <p class="card-text text-secondary">If you have any problem or issue, send us a message.</p>
+                  <a class="btn btn-primary rounded-pill fw-bold" href="sms:">Message Us</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+        <button class="btn btn-sm rounded-pill fw-bold btn-primary mt-3 me-2 float-end" data-bs-target="#signin" data-bs-toggle="modal">Back to Sign In</button>
+      </div>
+    </div>
+  </div>
+</div> 
