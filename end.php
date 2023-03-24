7bb8a70022477a361bf9f27e5a9bef7ead7eb316
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-03-25 02:51:28 +0700

    Add files via upload

---
+++ end.php
@@ -0,0 +1,59 @@
+          <a href="profile.php" class="btn btn-danger ms-2 mt-2 fw-bold" role="button">back to profile</a>
+        </main> 
+      </div>
+    </div>
+    <style>
+      @media (max-width: 767px) {
+        .navbar-brand {
+          position: static;
+          display: block;
+          text-align: center;
+          margin: auto;
+          transform: none;
+        }
+
+        .navbar-brand {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          font-size: 18px;
+        }
+        
+        .text-nav {
+          text-align: ;
+        }
+      }
+
+      .navbar-brand {
+        font-size: 18px;
+      }
+      
+      @media (min-width: 992px) {
+        .navbar-toggler1 {
+          display: none;
+        }
+        
+        .text-nav {
+            text-align: left;
+        }
+      }
+    
+      .navbar-toggler1 {
+        background-color: #ededed;
+        border: none;
+        font-size: 8px;
+        margin-left: 8px;
+        border-radius: 5px;
+        padding: 6px;
+        transition: background-color 0.3s ease; 
+      }
+
+      .navbar-toggler1:hover {
+        background-color: rgba(0,0,0,0.2);
+      }  
+    </style>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ setheader.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Database Counts</title>
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  </head>
+  <body>
+    <nav class="navbar sticky-top navbar-light bg-white flex-md-nowrap p-0 shadow-sm" style="height: 45px;">
+      <div class="container-fluid">
+        <a class="navbar-brand text-secondary fw-bold" href="index.php">
+          ArtCODE
+        </a>
+        <button class="navbar-toggler1 d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <img src="icon/toggle1.svg" width="22" height="22">
+        </button>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
+          <div class="position-sticky pt-3 sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <h3 class="text-secondary text-center fw-bold">Settings</h3>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-nav fw-bold fs-5 <?php if(basename($_SERVER['PHP_SELF']) == 'setting.php') echo 'rounded-4 border border-4 bg-primary text-white' ?>" href="setting.php">
+                  <span class="align-text-bottom"></span>
+                  <i class="bi bi-gear-fill"></i> Settings
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-nav fw-bold fs-5 <?php if(basename($_SERVER['PHP_SELF']) == 'yourname.php') echo 'rounded-4 border border-4 bg-primary text-white' ?>" href="yourname.php">
+                  <span class="align-text-bottom"></span>
+                  <i class="bi bi-person-circle"></i> Username
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-nav fw-bold fs-5 <?php if(basename($_SERVER['PHP_SELF']) == 'propic.php') echo 'rounded-4 border border-4 bg-primary text-white' ?>" href="propic.php">
+                  <span class="align-text-bottom"></span>
+                  <i class="bi bi-person-square"></i> Picture
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-nav fw-bold fs-5 <?php if(basename($_SERVER['PHP_SELF']) == 'bg.php') echo 'rounded-4 border border-4 bg-primary text-white' ?>" href="bg.php">
+                  <span class="align-text-bottom"></span>
+                  <i class="bi bi-images"></i> Background
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-nav fw-bold fs-5 <?php if(basename($_SERVER['PHP_SELF']) == 'desc.php') echo 'rounded-4 border border-4 bg-primary text-white' ?>" href="desc.php">
+                  <span class="align-text-bottom"></span>
+                  <i class="bi bi-person-vcard"></i> Bio
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-nav fw-bold fs-5 <?php if(basename($_SERVER['PHP_SELF']) == 'setpass.php') echo 'rounded-4 border border-4 bg-primary text-white' ?>" href="setpass.php">
+                  <span class="align-text-bottom"></span>
+                  <i class="bi bi-key-fill"></i> Password
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-nav fw-bold fs-5 <?php if(basename($_SERVER['PHP_SELF']) == 'analytic.php') echo 'rounded-4 border border-4 bg-primary text-white' ?>" href="analytic.php">
+                  <span class="align-text-bottom"></span>
+                  <i class="bi bi-pie-chart-fill"></i> User's Analytic
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-nav fw-bold fs-5 <?php if(basename($_SERVER['PHP_SELF']) == 'setsupport.php') echo 'rounded-4 border border-4 bg-primary text-white' ?>" href="setsupport.php">
+                  <span class="align-text-bottom"></span>
+                  <i class="bi bi-headset"></i> Support
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        
