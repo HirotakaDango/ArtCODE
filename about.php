1948b632377359693ced497275bbe80b03dca72b
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-03-26 17:14:09 +0700

    add margin-bottom

---
+++ about.php
@@ -0,0 +1,58 @@
+  <div class="container">
+    <div class="rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Why ArtCODE?</h1>
+        <p class="col-md-8 fs-5 text-secondary fw-bold">ArtCODE is an online platform that combines the best features of Pixiv and Pinterest to create a unique and immersive experience for artists and art enthusiasts alike. Whether you're a seasoned professional or a budding beginner, ArtCODE provides a welcoming community where you can showcase your talent and connect with like-minded individuals from all over the world.</p>
+      </div>
+    </div>
+    <div class="row mb-5">
+      <div class="col-md-4 py-2">
+        <div class="card h-100">
+          <div class="card-body">
+            <i class="bi bi-speedometer2 text-primary fs-1 mb-3"></i>
+            <h3 class="fw-bold">Fast and Easy</h3>
+            <p class="text-secondary fw-bold">With ArtCODE, you can quickly and easily find the perfect artwork for your project or collection.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 py-2">
+        <div class="card h-100">
+          <div class="card-body">
+            <i class="bi bi-globe text-primary fs-1 mb-3"></i>
+            <h3 class="fw-bold">Global Community</h3>
+            <p class="text-secondary fw-bold">ArtCODE connects you with a global community of artists and art lovers, allowing you to discover new perspectives and inspiration from around the world.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 py-2">
+        <div class="card h-100">
+          <div class="card-body">
+            <i class="bi bi-award text-primary fs-1 mb-3"></i>
+            <h3 class="fw-bold">Quality and Diversity</h3>
+            <p class="text-secondary fw-bold">ArtCODE offers a diverse range of high-quality artworks, including traditional paintings, digital art, and more, to suit any taste and style.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row align-items-md-stretch py-3">
+      <div class="col-md-6 mb-down">
+        <div class="h-100 p-5 text-white bg-dark rounded-3">
+          <h2 class="fw-bold">Best Platform To Share Your Ideas</h2>
+          <p class="fw-bold">With our user-friendly interface, you can easily upload and organize your artwork into collections, share your creations with others, and discover new artists and artwork through our sophisticated recommendation system. You can also connect with other members of the community through comments and private messaging, participate in contests and events to showcase your work and win prizes, and gain exposure for your art through our social media channels.</p> 
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="h-100 p-5 bg-light border rounded-3">
+          <h2 class="fw-bold">We're Supporting Your Artworks</h2>
+          <p class="fw-bold">Our team is composed of passionate artists and developers who are dedicated to making ArtCODE the premier online destination for art lovers. We value creativity, diversity, and inclusivity, and we strive to create a platform that is accessible and enjoyable for everyone.</p>
+        </div>
+      </div>
+    </div>
+    <style>
+      @media (max-width: 767px) {
+        .mb-down {
+          margin-bottom: 12px;
+        }
+      }
+    </style>
+  </div>
