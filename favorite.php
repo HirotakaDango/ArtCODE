25d7663fd5f1efb1c1f7561b43752a878fd190d0
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-04-10 13:36:55 +0700

    Update url to use image id rather than filename

---
+++ favorite.php
@@ -0,0 +1,190 @@
+<?php
+  session_start();
+  if (!isset($_SESSION['email'])) {
+    header("Location: session.php");
+    exit;
+  }
+
+  // Connect to the SQLite database
+  $db = new SQLite3('database.sqlite');
+
+  // Get all of the favorite images for the current user
+  $email = $_SESSION['email'];
+  $result = $db->query("SELECT images.* FROM images INNER JOIN favorites ON images.id = favorites.image_id WHERE favorites.email = '$email' ORDER BY favorites.id DESC");
+
+  // Process any favorite/unfavorite requests
+  if (isset($_POST['favorite'])) {
+    $image_id = $_POST['image_id'];
+    
+    // Check if the image has already been favorited by the current user
+    $existing_fav = $db->querySingle("SELECT COUNT(*) FROM favorites WHERE email = '$email' AND image_id = $image_id");
+    
+    if ($existing_fav == 0) {
+      $db->exec("INSERT INTO favorites (email, image_id) VALUES ('$email', $image_id)");
+    }
+    
+    // Redirect to the same page to prevent duplicate form submissions
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit();
+    
+  } elseif (isset($_POST['unfavorite'])) {
+    $image_id = $_POST['image_id'];
+    $db->exec("DELETE FROM favorites WHERE email = '$email' AND image_id = $image_id");
+    
+    // Redirect to the same page to prevent duplicate form submissions
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit();
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ArtCODE</title>
+    <link rel="manifest" href="manifest.json">
+    <link rel="icon" type="image/png" href="icon/favicon.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+  </head>
+  <body>
+    <?php include('header.php'); ?>
+    <style>
+      .image-container {
+        margin-bottom: -24px;  
+      }
+      
+      .images {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+        grid-gap: 2px;
+        justify-content: center;
+        margin-right: 3px;
+        margin-left: 3px;
+      }
+
+      .images a {
+        display: block;
+        border-radius: 4px;
+        overflow: hidden;
+        border: 2px solid #ccc;
+      }
+
+      .images img {
+        width: 100%;
+        height: auto;
+        object-fit: cover;
+        height: 200px;
+        transition: transform 0.5s ease-in-out;
+      }
+    
+      @media (min-width: 768px) {
+        .p-b3 {
+          margin-left: 6px;
+          border-radius: 4px;
+          margin-top: -71px;
+        } 
+      }
+      
+      @media (max-width: 767px) {
+        .p-b3 {
+          margin-left: 5px;
+          border-radius: 4px;
+          margin-top: -71px;
+        }
+      } 
+
+      @media (max-width: 450px) {
+        .p-b3 {
+          margin-left: 6px;
+          border-radius: 4px;
+          margin-top: -70px;
+        } 
+      }
+
+      @media (max-width: 415px) {
+        .p-b3 {
+          margin-left: 6px;
+          border-radius: 4px;
+          margin-top: -70px;
+        } 
+      }
+
+      @media (max-width: 380px) {
+        .p-b3 {
+          margin-left: 6px;
+          border-radius: 4px;
+          margin-top: -70px;
+        } 
+      }
+    </style>
+    <div class="mt-2">
+      <h5 class="text-center text-secondary fw-bold">MY FAVORITES</h5>
+      <div class="images">
+        <?php while ($image = $result->fetchArray()): ?>
+            <div class="image-container">
+              <a href="image.php?filename=<?php echo $image['id']; ?>">
+                <img class="card-img-top lazy-load" data-src="thumbnails/<?php echo $image['filename']; ?>">
+              </a>  
+              <div class="card-body">
+                <form method="POST">
+                  <input type="hidden" name="image_id" value="<?php echo $image['id']; ?>">
+                  <button type="submit" class="btn p-b3 btn-sm btn-dark opacity-50 rounded fw-bold" name="unfavorite"><i class="bi bi-heart-fill"></i></button>
+                </form>
+              </div>
+            </div>
+        <?php endwhile; ?> 
+      </div>
+    </div>
+    <script>
+      document.addEventListener("DOMContentLoaded", function() {
+        let lazyloadImages;
+        if("IntersectionObserver" in window) {
+          lazyloadImages = document.querySelectorAll(".lazy-load");
+          let imageObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+              if(entry.isIntersecting) {
+                let image = entry.target;
+                image.src = image.dataset.src;
+                image.classList.remove("lazy-load");
+                imageObserver.unobserve(image);
+              }
+            });
+          });
+          lazyloadImages.forEach(function(image) {
+            imageObserver.observe(image);
+          });
+        } else {
+          let lazyloadThrottleTimeout;
+          lazyloadImages = document.querySelectorAll(".lazy-load");
+
+          function lazyload() {
+            if(lazyloadThrottleTimeout) {
+              clearTimeout(lazyloadThrottleTimeout);
+            }
+            lazyloadThrottleTimeout = setTimeout(function() {
+              let scrollTop = window.pageYOffset;
+              lazyloadImages.forEach(function(img) {
+                if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                  img.src = img.dataset.src;
+                  img.classList.remove('lazy-load');
+                }
+              });
+              if(lazyloadImages.length == 0) {
+                document.removeEventListener("scroll", lazyload);
+                window.removeEventListener("resize", lazyload);
+                window.removeEventListener("orientationChange", lazyload);
+              }
+            }, 20);
+          }
+          document.addEventListener("scroll", lazyload);
+          window.addEventListener("resize", lazyload);
+          window.addEventListener("orientationChange", lazyload);
+        }
+      })
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ global_asc.php
@@ -0,0 +1,187 @@
+<?php
+$email = $_SESSION['email'];
+
+// Connect to the SQLite database using parameterized query
+$db = new SQLite3('database.sqlite');
+
+// Pagination variables
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$limit = 30;
+$offset = ($page - 1) * $limit;
+
+// Get the total number of images from the database
+$countStmt = $db->prepare("SELECT COUNT(*) FROM images");
+$countResult = $countStmt->execute();
+$total = $countResult->fetchArray()[0];
+
+// Get 25 images from the database using parameterized query
+$stmt = $db->prepare("SELECT images.*, users.email FROM images INNER JOIN users ON images.email = users.email ORDER BY images.id ASC LIMIT :limit OFFSET :offset");
+$stmt->bindValue(':limit', $limit, SQLITE3_INTEGER);
+$stmt->bindValue(':offset', $offset, SQLITE3_INTEGER);
+$result = $stmt->execute();
+?>
+
+  <body>
+    <div class="dropdown mt-2 mb-2">
+      <button class="btn btn-sm fw-bold rounded-pill ms-2 btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="bi bi-images"></i> sort by
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item fw-bold" href="?by=newest&page=<?php echo isset($_GET['page']) ? $_GET['page'] : '1'; ?>">Newest</a></li>
+        <li><a class="dropdown-item fw-bold active" href="?by=oldest&page=<?php echo isset($_GET['page']) ? $_GET['page'] : '1'; ?>">Oldest</a></li>
+      </ul>
+    </div>
+    <div class="content">
+      <?php while ($image = $result->fetchArray()): ?>
+        <?php
+          $title = $image['title'];
+          $filename = $image['filename'];
+          $email = $image['email'];
+          $artist = '';
+          $stmt = $db->prepare("SELECT id, artist FROM users WHERE email = ?");
+          $stmt->bindValue(1, $email, SQLITE3_TEXT);
+          $result2 = $stmt->execute();
+          if ($user = $result2->fetchArray()) {
+            $artist = $user['artist'];
+            $id = $user['id'];
+          }
+        ?>
+        <div class="image">
+          <a class="img-block" href="image.php?filename=<?php echo $image['id']; ?>">
+            <img class="lazy-load" data-src="thumbnails/<?php echo $image['filename']; ?>">
+          </a>
+          <div>
+            <h5 class="text-white ms-2 fw-bold" style="margin-top: -76px; text-shadow: 1px 1px 1px #000;"><?= $title ?></h5>
+            <?php if ($artist != ''): ?>
+              <p class="text-white text-decoration-none fw-bold ms-2" style="text-shadow: 1px 1px 1px #000;">uploaded by <a class="text-decoration-none text-white btn btn-sm btn-secondary rounded-pill opacity-75 fw-bold mb-1" href="artist.php?id=<?= $id ?>"><?php echo $artist; ?></a></p>
+            <?php endif; ?>
+          </div>
+        </div>
+      <?php endwhile; ?>
+    </div>
+    <div class="mt-5 mb-2 d-flex justify-content-center btn-toolbar container">
+      <?php
+        $totalPages = ceil($total / $limit);
+        $prevPage = $page - 1;
+        $nextPage = $page + 1;
+
+        if ($page > 1) {
+          echo '<a type="button" class="btn rounded-pill fw-bold btn-secondary opacity-50 me-1" href="?by=oldest&page=' . $prevPage . '"><i class="bi bi-arrow-left-circle-fill"></i> prev</a>';
+        }
+        if ($page < $totalPages) {
+          echo '<a type="button" class="btn rounded-pill fw-bold btn-secondary opacity-50 ms-1" href="?by=oldest&page=' . $nextPage . '">next <i class="bi bi-arrow-right-circle-fill"></i></a>';
+        }
+      ?>
+    </div>
+    <div class="mt-5"></div>  
+    <style>
+      body {
+        margin: 0;
+      }
+      
+      .content {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 2px;
+        margin-left: -2px; /* Remove gap on left edge */
+        margin-right: -2px; 
+      }
+      
+      .image {
+        width: 100%;
+        width: calc(33.33% - 2px);
+        margin: 0;
+        padding: auto;
+        box-sizing: border-box;
+      }
+      
+      .img-block {
+        display: block;
+        width: auto;
+        margin-bottom: 17px;
+      }
+      
+      .image img {
+        width: 100%;
+        height: 400px;
+        object-fit: cover;
+      }
+      
+      .image h5, .image p {
+        margin: 0;
+      }
+      
+    
+      @media screen and (max-width: 767px) {
+        .content {
+          margin-left: 0;
+          margin-right: 0;
+        }
+        
+        .image {
+          width: 100%;
+          margin: 0;
+          border-top: 2px solid lightgray;
+          border-bottom: 2px solid lightgray;
+        }
+      }
+    </style> 
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+          let lazyloadImages;
+          if("IntersectionObserver" in window) {
+            lazyloadImages = document.querySelectorAll(".lazy-load");
+            let imageObserver = new IntersectionObserver(function(entries, observer) {
+              entries.forEach(function(entry) {
+                if(entry.isIntersecting) {
+                  let image = entry.target;
+                  image.src = image.dataset.src;
+                  image.classList.remove("lazy-load");
+                  imageObserver.unobserve(image);
+                }
+              });
+            });
+            lazyloadImages.forEach(function(image) {
+              imageObserver.observe(image);
+            });
+          } else {
+            let lazyloadThrottleTimeout;
+            lazyloadImages = document.querySelectorAll(".lazy-load");
+
+            function lazyload() {
+              if(lazyloadThrottleTimeout) {
+                clearTimeout(lazyloadThrottleTimeout);
+              }
+              lazyloadThrottleTimeout = setTimeout(function() {
+                let scrollTop = window.pageYOffset;
+                lazyloadImages.forEach(function(img) {
+                  if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                    img.src = img.dataset.src;
+                    img.classList.remove('lazy-load');
+                  }
+                });
+                if(lazyloadImages.length == 0) {
+                  document.removeEventListener("scroll", lazyload);
+                  window.removeEventListener("resize", lazyload);
+                  window.removeEventListener("orientationChange", lazyload);
+                }
+              }, 20);
+            }
+            document.addEventListener("scroll", lazyload);
+            window.addEventListener("resize", lazyload);
+            window.addEventListener("orientationChange", lazyload);
+          }
+        })
+    </script>
+    <script>
+        if ('serviceWorker' in navigator) {
+          window.addEventListener('load', function() {
+            navigator.serviceWorker.register('sw.js').then(function(registration) {
+              console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function(err) {
+              console.log('ServiceWorker registration failed: ', err);
+            });
+          });
+        }
+    </script>
+  </body>
