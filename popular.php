aa2c199d0390be6dc7110b3e8133e3f48e9f43ab
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-06-11 16:47:59 +0700

    Add files via upload

---
+++ popular.php
@@ -0,0 +1,321 @@
+<?php
+session_start();
+if (!isset($_SESSION['email'])) {
+  header("Location: session.php");
+  exit;
+}
+
+$dbP = new SQLite3('database.sqlite'); 
+
+// Define the number of images to display per page
+$limit = 100;
+
+// Get the current page number from the query string
+if (isset($_GET['page'])) {
+  $page = $_GET['page'];
+} else {
+  $page = 1;
+}
+
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+// Calculate the offset value for the SQL query
+$offset = ($page - 1) * $limit;
+
+// Process any favorite/unfavorite requests
+if (isset($_POST['favorite'])) {
+  $image_id = $_POST['image_id'];
+
+  // Check if the image has already been favorited by the current user
+  $existing_fav = $dbP->querySingle("SELECT COUNT(*) FROM favorites WHERE email = '{$_SESSION['email']}' AND image_id = $image_id");
+
+  if ($existing_fav == 0) {
+    $dbP->exec("INSERT INTO favorites (email, image_id) VALUES ('{$_SESSION['email']}', $image_id)");
+  }
+
+  // Redirect to the same page to prevent duplicate form submissions
+  header("Location: popular.php?page=$page");
+  exit();
+
+} elseif (isset($_POST['unfavorite'])) {
+  $image_id = $_POST['image_id'];
+  $dbP->exec("DELETE FROM favorites WHERE email = '{$_SESSION['email']}' AND image_id = $image_id");
+
+  // Redirect to the same page to prevent duplicate form submissions
+  header("Location: popular.php?page=$page"); 
+  exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ArtCODE</title>
+    <link rel="icon" type="image/png" href="icon/favicon.png">
+    <?php include('bootstrapcss.php'); ?>
+    <style>
+      .images {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr); /* Two columns in mobile view */
+        grid-gap: 3px;
+        justify-content: center;
+        margin-right: 3px;
+        margin-left: 3px;
+      }
+
+      @media (min-width: 768px) {
+        /* For desktop view, change the grid layout */
+        .images {
+          grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+        }
+      }
+
+      .imagesA {
+        display: block;
+        overflow: hidden;
+      }
+
+      .imagesImg {
+        width: 100%;
+        height: auto;
+        object-fit: cover;
+        height: 200px;
+        transition: transform 0.5s ease-in-out;
+      }
+    </style>
+  </head>
+  <body>
+    <?php include('header.php'); ?>
+    <h5 class="ms-2 mt-2 text-secondary fw-bold"><i class="bi bi-images"></i> Popular Images</h5>
+    <div class="images mb-2">
+      <?php
+        $email = $_SESSION['email'];
+
+        // Get the total number of images
+        $total = $dbP->querySingle("SELECT COUNT(*) FROM images");
+
+        // Calculate the total number of pages
+        $totalPages = ceil($total / $limit);
+ 
+        // Get the images for the current page using a parameterized query with LIMIT and OFFSET clauses
+        $stmtP = $dbP->prepare("SELECT images.id, images.filename, images.tags, images.imgdesc, images.title, COUNT(favorites.id) AS favorite_count FROM images LEFT JOIN favorites ON images.id = favorites.image_id GROUP BY images.id ORDER BY favorite_count DESC LIMIT :limit OFFSET :offset");
+        $stmtP->bindValue(':limit', $limit, SQLITE3_INTEGER);
+        $stmtP->bindValue(':offset', $offset, SQLITE3_INTEGER);
+        $resultP = $stmtP->execute();
+
+        // Display the images for the current page
+        while ($imageP = $resultP->fetchArray()):
+      ?>
+        
+        <div class="image-container">
+          <div class="position-relative">
+            <a class="shadow rounded imagesA" href="image.php?artworkid=<?php echo $imageP['id']; ?>">
+              <img class="lazy-load imagesImg" data-src="thumbnails/<?php echo $imageP['filename']; ?>" alt="<?php echo $imageP['title']; ?>">
+            </a>
+            <div class="position-absolute top-0 start-0">
+              <div class="dropdown">
+                <button class="btn btn-sm btn-dark ms-1 mt-1 rounded-1 opacity-50" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class="bi bi-three-dots-vertical"></i>
+                </button>
+                <ul class="dropdown-menu">
+                  <?php
+                    $stmt = $dbP->prepare("SELECT COUNT(*) FROM favorites WHERE email = ? AND image_id = ?");
+                    $stmt->bindValue(1, $email, SQLITE3_TEXT);
+                    $stmt->bindValue(2, $imageP['id'], SQLITE3_INTEGER);
+                    $result = $stmt->execute()->fetchArray();
+                    $is_favorited = $result[0];
+                    if ($is_favorited) {
+                  ?>
+                    <form method="POST">
+                      <input type="hidden" name="image_id" value="<?php echo $imageP['id']; ?>">
+                      <li><button type="submit" class="dropdown-item fw-bold" name="unfavorite"><i class="bi bi-heart-fill"></i> <small>unfavorite</small></button></li>
+                    </form>
+                  <?php } else { ?>
+                    <form method="POST">
+                      <input type="hidden" name="image_id" value="<?php echo $imageP['id']; ?>">
+                      <li><button type="submit" class="dropdown-item fw-bold" name="favorite"><i class="bi bi-heart"></i> <small>favorite</small></button></li>
+                    </form>
+                  <?php } ?>
+                  <li><button class="dropdown-item fw-bold" onclick="shareImage(<?php echo $imageP['id']; ?>)"><i class="bi bi-share-fill"></i> <small>share</small></button></li>
+                  <li><button class="dropdown-item fw-bold" data-bs-toggle="modal" data-bs-target="#infoImage_<?php echo $imageP['id']; ?>"><i class="bi bi-info-circle-fill"></i> <small>info</small></button></li>
+                </ul>
+                <!-- Modal -->
+                <div class="modal fade" id="infoImage_<?php echo $imageP['id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog modal-fullscreen" role="document">
+                    <div class="modal-content shadow">
+                      <div class="modal-body p-4 text-center">
+                        <h5 class="modal-title fw-bold text-start mb-2"><?php echo $imageP['title']?></h5>
+                        <div class="row featurette">
+                          <div class="col-md-5 order-md-1 mb-2">
+                            <a href="image.php?artworkid=<?php echo $imageP['id']; ?>">
+                              <img class="rounded object-fit-cover mb-3 shadow lazy-load" data-src="thumbnails/<?php echo $imageP['filename']; ?>" alt="<?php echo $imageP['title']; ?>" style="width: 100%; height: 100%;">
+                            </a>
+                          </div>
+                          <div class="col-md-7 order-md-2">
+                            <button type="button" class="btn btn-secondary rounded w-100 fw-bold opacity-50 mb-2" data-bs-dismiss="modal">close</button>
+                            <p class="text-start fw-semibold">share to:</p>
+                            <div class="btn-group w-100 mb-2" role="group" aria-label="Share Buttons">
+                              <!-- Twitter -->
+                              <a class="btn btn-outline-dark" href="https://twitter.com/intent/tweet?url=<?php echo (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . '/image.php?artworkid=' . $imageP['id']; ?>" target="_blank" rel="noopener noreferrer">
+                                <i class="bi bi-twitter"></i>
+                              </a>
+                              
+                              <!-- Line -->
+                              <a class="btn btn-outline-dark" href="https://social-plugins.line.me/lineit/share?url=<?php echo (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . '/image.php?artworkid=' . $imageP['id']; ?>" target="_blank" rel="noopener noreferrer">
+                                <i class="bi bi-line"></i>
+                              </a>
+                              
+                              <!-- Email -->
+                              <a class="btn btn-outline-dark" href="mailto:?body=<?php echo (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . '/image.php?artworkid=' . $imageP['id']; ?>">
+                                <i class="bi bi-envelope-fill"></i>
+                              </a>
+                              
+                              <!-- Reddit -->
+                              <a class="btn btn-outline-dark" href="https://www.reddit.com/submit?url=<?php echo (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . '/image.php?artworkid=' . $imageP['id']; ?>" target="_blank" rel="noopener noreferrer">
+                                <i class="bi bi-reddit"></i>
+                              </a>
+                              
+                              <!-- Instagram -->
+                              <a class="btn btn-outline-dark" href="https://www.instagram.com/?url=<?php echo (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . '/image.php?artworkid=' . $imageP['id']; ?>" target="_blank" rel="noopener noreferrer">
+                                <i class="bi bi-instagram"></i>
+                              </a>
+                              
+                              <!-- Facebook -->
+                              <a class="btn btn-outline-dark" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . '/image.php?artworkid=' . $imageP['id']; ?>" target="_blank" rel="noopener noreferrer">
+                                <i class="bi bi-facebook"></i>
+                              </a>
+                            </div>
+                            <p class="text-start fw-semibold">
+                              <?php
+                                $messageText = $imageP['imgdesc'];
+                                $messageTextWithoutTags = strip_tags($messageText);
+                                $pattern = '/\bhttps?:\/\/\S+/i';
+
+                                $formattedText = preg_replace_callback($pattern, function ($matches) {
+                                  $url = htmlspecialchars($matches[0]);
+                                  return '<a href="' . $url . '">' . $url . '</a>';
+                                }, $messageTextWithoutTags);
+
+                                $formattedTextWithLineBreaks = nl2br($formattedText);
+                                echo $formattedTextWithLineBreaks;
+                              ?>
+                            </p>
+                            <div class="card container">
+                              <p class="text-center fw-semibold mt-2">Image Information</p>
+                              <p class="text-start fw-semibold">Image ID: "<?php echo $imageP['id']?>"</p>
+                              <?php
+                                // Get image size in megabytes
+                                $image_size = round(filesize('images/' . $imageP['filename']) / (1024 * 1024), 2);
+
+                                // Get image dimensions
+                                list($width, $height) = getimagesize('images/' . $imageP['filename']);
+                            
+                                // Display image information
+                                echo "<p class='text-start fw-semibold'>Image data size: " . $image_size . " MB</p>";
+                                echo "<p class='text-start fw-semibold'>Image dimensions: " . $width . "x" . $height . "</p>";
+                                echo "<p class='text-start fw-semibold'><a class='text-decoration-none' href='images/" . $imageP['filename'] . "'>View original image</a></p>";
+                              ?>
+                            </div>
+                            <div class="btn-group w-100 mt-2">
+                              <a class="btn btn-primary fw-bold rounded-start-5" href="image.php?artworkid=<?php echo $imageP['id']; ?>"><i class="bi bi-eye-fill"></i> view</a>
+                              <a class="btn btn-primary fw-bold" href="images/<?php echo $imageP['filename']; ?>" download><i class="bi bi-download"></i> download</a>
+                              <button class="btn btn-primary fw-bold rounded-end-5" onclick="shareImage(<?php echo $imageP['id']; ?>)"><i class="bi bi-share-fill"></i> share</button>
+                            </div> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      <?php endwhile; ?>
+    </div> 
+    <div class="mt-5 mb-2 d-flex justify-content-center btn-toolbar container">
+      <?php
+        $totalPages = ceil($total / $limit);
+        $prevPage = $page - 1;
+        $nextPage = $page + 1;
+
+        if ($page > 1) {
+          echo '<a type="button" class="btn rounded-pill fw-bold btn-secondary opacity-50 me-1" href="?page=' . $prevPage . '"><i class="bi bi-arrow-left-circle-fill"></i> prev</a>';
+        }
+        if ($page < $totalPages) {
+          echo '<a type="button" class="btn rounded-pill fw-bold btn-secondary opacity-50 ms-1" href="?page=' . $nextPage . '">next <i class="bi bi-arrow-right-circle-fill"></i></a>';
+        }
+      ?>
+    </div>
+    <script>
+      function shareImage(userId) {
+        // Compose the share URL
+        var shareUrl = 'image.php?artworkid=' + userId;
+
+        // Check if the Share API is supported by the browser
+        if (navigator.share) {
+          navigator.share({
+          url: shareUrl
+        })
+          .then(() => console.log('Shared successfully.'))
+          .catch((error) => console.error('Error sharing:', error));
+        } else {
+          console.log('Share API is not supported in this browser.');
+          // Provide an alternative action for browsers that do not support the Share API
+          // For example, you can open a new window with the share URL
+          window.open(shareUrl, '_blank');
+        }
+      }
+    </script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+          let lazyloadImages;
+          if("IntersectionObserver" in window) {
+            lazyloadImages = document.querySelectorAll(".lazy-load");
+            let imageObserver = new IntersectionObserver(function(entries, observer) {
+              entries.forEach(function(entry) {
+                if(entry.isIntersecting) {
+                  let image = entry.target;
+                  image.src = image.dataset.src;
+                  image.classList.remove("lazy-load");
+                  imageObserver.unobserve(image);
+                }
+              });
+            });
+            lazyloadImages.forEach(function(image) {
+              imageObserver.observe(image);
+            });
+          } else {
+            let lazyloadThrottleTimeout;
+            lazyloadImages = document.querySelectorAll(".lazy-load");
+
+            function lazyload() {
+              if(lazyloadThrottleTimeout) {
+                clearTimeout(lazyloadThrottleTimeout);
+              }
+              lazyloadThrottleTimeout = setTimeout(function() {
+                let scrollTop = window.pageYOffset;
+                lazyloadImages.forEach(function(img) {
+                  if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                    img.src = img.dataset.src;
+                    img.classList.remove('lazy-load');
+                  }
+                });
+                if(lazyloadImages.length == 0) {
+                  document.removeEventListener("scroll", lazyload);
+                  window.removeEventListener("resize", lazyload);
+                  window.removeEventListener("orientationChange", lazyload);
+                }
+              }, 20);
+            }
+            document.addEventListener("scroll", lazyload);
+            window.addEventListener("resize", lazyload);
+            window.addEventListener("orientationChange", lazyload);
+          }
+        })
+    </script>
+    <?php include('bootstrapjs.php'); ?>
+  </body>
+</html>
---
+++ reply_comments_preview.php
@@ -0,0 +1,203 @@
+<?php
+// Start the session
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['email'])) {
+  // Redirect to the login page if not
+  header('Location: session.php');
+  exit();
+}
+
+// Open the SQLite database
+$db = new SQLite3('database.sqlite');
+ 
+// Create the reply_comments table if it doesn't exist
+$db->exec('CREATE TABLE IF NOT EXISTS reply_comments (id INTEGER PRIMARY KEY AUTOINCREMENT, comment_id INTEGER, email TEXT, reply TEXT, FOREIGN KEY (comment_id) REFERENCES comments(id))');
+
+// Get the image id from comment.php
+$imageid = $_GET['imageid'];
+
+// Get the id of the image
+$stmt = $db->prepare("SELECT * FROM images WHERE id=:imageid");
+$stmt->bindValue(':imageid', $imageid, SQLITE3_INTEGER);
+$image = $stmt->execute()->fetchArray(SQLITE3_ASSOC);
+
+// Check if the reply form was submitted
+if (isset($_POST['reply_comment_id'], $_POST['reply'])) {
+  // Trim the reply text to remove leading and trailing spaces
+  $reply = trim($_POST['reply']);
+
+  // Check if the reply is empty after trimming
+  if (!empty($reply)) {
+    // Prepare the reply text by removing special characters and converting newlines to <br> tags
+    $reply = nl2br(filter_var($reply, FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_FLAG_STRIP_LOW));
+
+    // Insert a new reply into the reply_comments table
+    $stmt = $db->prepare('INSERT INTO reply_comments (comment_id, email, reply) VALUES (?, ?, ?)');
+    $stmt->bindValue(1, $_POST['reply_comment_id'], SQLITE3_INTEGER);
+    $stmt->bindValue(2, $_SESSION['email'], SQLITE3_TEXT);
+    $stmt->bindValue(3, $reply, SQLITE3_TEXT);
+    $stmt->execute();
+
+    // Redirect back to the current page with the comment_id parameter
+    header('Location: reply_comments_preview.php?imageid='.$imageid.'&comment_id=' . $_POST['reply_comment_id']);
+    exit();
+  } else {
+    // Handle the case where the reply is empty
+    // Display an error message or take appropriate action
+  }
+}
+
+// Check if the "delete_reply_id" key is set in the $_GET superglobal
+if (isset($_GET['delete_reply_id'])) {
+  // Get the comment_id and image_id for the reply to be deleted
+  $get_reply_info_stmt = $db->prepare('SELECT comment_id FROM reply_comments WHERE id = ?');
+  $get_reply_info_stmt->bindValue(1, $_GET['delete_reply_id'], SQLITE3_INTEGER);
+  $reply_info_result = $get_reply_info_stmt->execute()->fetchArray(SQLITE3_ASSOC);
+  $imageid = $_GET['imageid'];
+  
+
+  if ($reply_info_result !== false) {
+    $comment_id = $reply_info_result['comment_id'];
+    // $image_id = $_GET['imageid'];
+    // $image_id = $reply_info_result['image_id'];
+
+    // Delete the selected reply from the reply_comments table
+    $delete_reply_stmt = $db->prepare('DELETE FROM reply_comments WHERE id = ?');
+    $delete_reply_stmt->bindValue(1, $_GET['delete_reply_id'], SQLITE3_INTEGER);
+    $delete_reply_stmt->execute();
+
+    // Redirect back to the current page with the imageid and comment_id parameters
+    $redirect_url = 'reply_comments_preview.php?imageid=' . urlencode($imageid) . '&comment_id=' . urlencode($comment_id);
+    header('Location: ' . $redirect_url);
+    exit();
+  } else {
+    // Handle the case where the comment_id or image_id could not be retrieved
+  }
+}
+
+// Get the selected comment based on its ID
+$comment_id = isset($_GET['comment_id']) ? $_GET['comment_id'] : null;
+if ($comment_id !== null) {
+  $comment = $db->prepare('SELECT * FROM comments WHERE id = ?');
+  $comment->bindValue(1, $comment_id, SQLITE3_INTEGER);
+  $comment = $comment->execute()->fetchArray(SQLITE3_ASSOC);
+
+  // Get all replies for the selected comment from the reply_comments table, along with the user information
+  $replies = $db->prepare('SELECT rc.*, u.artist, u.id as userid FROM reply_comments rc JOIN users u ON rc.email = u.email WHERE rc.comment_id = ? ORDER BY rc.id DESC');
+  $replies->bindValue(1, $comment_id, SQLITE3_INTEGER);
+  $replies = $replies->execute();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Reply Section</title>
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="icon/favicon.png">
+    <?php include('bootstrapcss.php'); ?>
+  </head>
+  <body>
+    <div class="mb-3">
+      <?php if ($comment_id !== null && $comment !== false): ?>
+      <div class="modal-content border-3 border-bottom ">
+        <div class="modal-body p-3">
+          <h5 class="mb-0 fw-bold text-center">Comment Replies</h5>
+          <div class="fw-bold mt-2">
+            <small>
+              <?php
+                $messageText = $comment['comment'];
+                $messageTextWithoutTags = strip_tags($messageText);
+                $pattern = '/\bhttps?:\/\/\S+/i';
+
+                $formattedText = preg_replace_callback($pattern, function ($matches) {
+                  $url = htmlspecialchars($matches[0]);
+                  return '<a href="' . $url . '">' . $url . '</a>';
+                }, $messageTextWithoutTags);
+
+                $formattedTextWithLineBreaks = nl2br($formattedText);
+                echo $formattedTextWithLineBreaks;
+              ?>
+            </small>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div>
+        <?php
+          // Display each reply and a delete button
+          while ($reply = $replies->fetchArray(SQLITE3_ASSOC)):
+        ?>
+          <div class="card mb-2">
+            <div class="ms-1">
+              <p class="text-dark fw-bold mt-1">
+                <i class="bi bi-person-circle"></i>
+                <a class="text-dark text-decoration-none" href="artist.php?id=<?php echo $reply['userid']; ?>" target="_blank">@<?php echo $reply['artist']; ?></a>
+              </p>
+              <div class="text-secondary fw-bold mb-2" style="word-break: break-word;" data-lazyload>
+                <small>
+                  <?php
+                    $messageText = $reply['reply'];
+                    $messageTextWithoutTags = strip_tags($messageText);
+                    $pattern = '/\bhttps?:\/\/\S+/i';
+
+                    $formattedText = preg_replace_callback($pattern, function ($matches) {
+                      $url = htmlspecialchars($matches[0]);
+                      return '<a href="' . $url . '">' . $url . '</a>';
+                    }, $messageTextWithoutTags);
+
+                    $formattedTextWithLineBreaks = nl2br($formattedText);
+                    echo $formattedTextWithLineBreaks;
+                  ?>
+                </small>
+              </div>
+              <?php if ($_SESSION['email'] === $reply['email']): ?>
+                <form action="" method="get">
+                  <div class="btn-group position-absolute top-0 end-0 mt-1 me-1 opacity-50">
+                    <a href="edit_reply_comments_preview.php?reply_id=<?php echo $reply['id']; ?>&imageid=<?php echo $imageid; ?>" class="btn btn-sm btn-secondary"><i class="bi bi-pencil-fill"></i></a>
+                    <input type="hidden" name="delete_reply_id" value="<?= $reply['id'] ?>">
+                    <input type="hidden" name="imageid" value="<?= $imageid ?>" />
+                    <button onclick="return confirm('Are you sure?')" class="btn btn-sm btn-secondary " type="submit">
+                      <i class="bi bi-trash-fill"></i>
+                    </button>
+                  </div>
+                </form>
+              <?php endif; ?>
+            </div>
+          </div>
+        <?php endwhile; ?>
+      <?php else:
+        // Display an error message if the comment ID is invalid
+        echo "<p>Invalid comment ID.</p>";
+      endif; ?>
+      <div class="fixed-bottom w-100">
+        <form action="" method="POST">
+          <div class="input-group w-100">
+            <input type="hidden" name="reply_comment_id" value="<?= $comment['id'] ?>">
+            <textarea id="message-input" name="reply" class="form-control rounded-top-0 rounded-bottom-1 rounded-end-0 fw-semibold border-start-0 border-bottom-0" style="height: 40px; max-height: 150px;" placeholder="Type your comment..." aria-label="Type a message..." aria-describedby="basic-addon2" 
+              onkeydown="if(event.keyCode == 13) { this.style.height = (parseInt(this.style.height) + 10) + 'px'; return true; }"
+              onkeyup="this.style.height = '40px'; var newHeight = (this.scrollHeight + 10 * (this.value.split(/\r?\n/).length - 1)) + 'px'; if (parseInt(newHeight) > 150) { this.style.height = '150px'; } else { this.style.height = newHeight; }"></textarea>
+            <button type="submit" class="btn btn-primary rounded-top-0 fw-bold">send</button>
+          </div>
+        </form> 
+      </div>
+    </div>
+    <br><br><br>
+    <div class="ms-2 d-none-sm position-absolute top-0 mt-2 start-0">
+      <button class="btn btn-sm btn-secondary rounded-pill fw-bold opacity-50" onclick="goBack()">
+        <i class="bi bi-arrow-left-circle-fill"></i> Back
+      </button>
+    </div>
+    <script>
+      function goBack() {
+        window.location.href = "comment_preview.php?imageid=<?php echo $imageid;; ?>";
+        // window.location.href = "index.php";
+      }
+    </script>
+    <?php include('bootstrapjs.php'); ?>
+  </body>
+</html>
