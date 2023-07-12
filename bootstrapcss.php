5f5ca13c7de506c5abed0bc987d6359544de447a
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-07-12 12:32:02 +0700

    Delete bootstrapcss.php

--- bootstrapcss.php
+++
@@ -1,2 +0,0 @@
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
