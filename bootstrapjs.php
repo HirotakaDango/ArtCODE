f74659354fe781ff31ce874b18635ba43cc4e888
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-07-30 18:05:30 +0700

    Delete bootstrapjs.php

--- bootstrapjs.php
+++
@@ -1,2 +0,0 @@
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
