b79b2110764e3b73fcecbbbfb06d93bc9d14f4cc
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-08-25 14:02:44 +0700

    Add files via upload

---
+++ settings/date.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['email'])) {
+  header("Location: ../session.php");
+  exit;
+}
+
+// Replace 'your-database-name.sqlite' with the actual path to your SQLite database file
+$database = new SQLite3('../database.sqlite');
+
+// Get the current born of the user from the database
+$query = "SELECT born FROM users WHERE email = :email";
+$statement = $database->prepare($query);
+$statement->bindValue(':email', $_SESSION['email']);
+$result = $statement->execute();
+$row = $result->fetchArray(SQLITE3_ASSOC);
+$currentborn = $row['born'];
+
+// Process the form submission if the user has selected a new born
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Get the selected born from the form
+  $selectedborn = $_POST['born'];
+
+  // Update the user's born in the database
+  $query = "UPDATE users SET born = :born WHERE email = :email";
+  $statement = $database->prepare($query);
+  $statement->bindValue(':born', $selectedborn);
+  $statement->bindValue(':email', $_SESSION['email']);
+  $statement->execute();
+
+  // Redirect the user to a success page or any other desired location
+  header("Location: date.php");
+  exit;
+}
+
+// Close the database connection
+$database->close();
+?>
+
+  <?php include ('setheader.php');?>
+  <div class="container mt-4">
+    <h3 class="text-center fw-bold"><i class="bi bi-calendar-fill"></i> Change Date</h3>
+    <p class="fw-semibold">Current date: <?php echo $currentborn; ?></p>
+    <form method="POST" action="">
+      <div class="input-group">
+        <input type="date" class="form-control fw-bold" name="born" placeholder="Select a date" value="<?php echo $currentborn; ?>" required>
+        <span class="input-group-text"><i class="bi bi-calendar"></i></span>
+      </div>
+      <button type="submit" class="btn btn-primary w-100 fw-bold mt-2">Save</button>
+    </form>
+  </div>
+  <?php include('end.php'); ?> 
