1fce52aa8fbf2abe3728851422903ffd916bd3df
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-12-22 08:19:51 +0700

    Add files via upload

--- feeds/music/play_all.php
+++ feeds/music/play_all.php
@@ -579,9 +579,9 @@ if (isset($_POST['favorite'])) {
               <?php if ($prevRow): ?>
                 <a href="play_all.php?album=<?php echo urlencode($prevRow['album']); ?>&id=<?php echo $prevRow['id']; ?>" class="btn float-end text-white"><i class="bi bi-skip-start-fill display-1"></i></a>
               <?php endif; ?>
-              <button class="text-decoration-none btn text-white d-md-none d-lg-none" data-bs-toggle="modal" data-bs-target="#playList">
+              <a class="text-decoration-none btn text-white d-md-none d-lg-none" href="#playList">
                 <i class="bi bi-music-note-list display-1"></i>
-              </button>
+              </a>
               <?php if ($nextRow): ?>
                 <a href="play_all.php?album=<?php echo urlencode($nextRow['album']); ?>&id=<?php echo $nextRow['id']; ?>" class="btn float-end text-white"><i class="bi bi-skip-end-fill display-1"></i></a>
               <?php endif; ?>
@@ -634,44 +634,24 @@ if (isset($_POST['favorite'])) {
           </div>
         </div>
         <div class="col-md-7 order-md-2">
-          <div class="modal fade d-md-none d-lg-none" id="playList" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-fullscreen modal-dialog-scrollable">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fw-bold fs-5" id="exampleModalLabel"><i class="bi bi-music-note-list"></i> all song lists</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  <div class="overflow-y-auto" id="autoHeightDivM" style="max-height: 100%;">
-                    <?php foreach ($allRows as $song): ?>
-                      <div id="songM_<?php echo $song['id']; ?>" class="d-flex justify-content-between align-items-center rounded-4 bg-dark-subtle bg-opacity-10 my-2 <?php echo ($song['id'] == $row['id']) ? 'rounded-4 bg-body-tertiary border border-opacity-25 border-light' : ''; ?>">
-                        <a class="text-decoration-none music text-start w-100 text-white btn fw-bold border-0" href="play_all.php?album=<?php echo urlencode($song['album']); ?>&id=<?php echo $song['id']; ?>">
-                          <?php echo $song['title']; ?><br>
-                          <small class="text-muted"><?php echo $song['artist']; ?> - <?php echo $song['album']; ?></small>
-                        </a>
-                        <div class="dropdown dropdown-menu-end">
-                          <button class="text-decoration-none text-white btn fw-bold border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-three-dots-vertical"></i></button>
-                          <ul class="dropdown-menu rounded-4">
-                            <li><button class="dropdown-item fw-medium" onclick="sharePageS('<?php echo $song['id']; ?>', '<?php echo $song['title']; ?>')"><i class="bi bi-share-fill"></i> share</button></li>
-                            <li><a class="dropdown-item fw-medium" href="artist.php?id=<?php echo $song['userid']; ?>"><i class="bi bi-person-fill"></i> show artist</a></li>
-                            <li><a class="dropdown-item fw-medium" href="album.php?album=<?php echo $song['album']; ?>"><i class="bi bi-disc-fill"></i> show album</a></li>
-                          </ul>
-                        </div>
-                      </div>
-                    <?php endforeach; ?>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> 
-          <div class="d-none d-md-block d-lg-block">
+          <div class="d-md-none d-lg-none" id="playList">
             <h3 class="text-start fw-bold mb-3"><i class="bi bi-music-note-list"></i> all song lists</h3>
-            <div class="overflow-y-auto" id="autoHeightDiv" style="max-height: 100%;">
+            <div class="overflow-y-auto" id="autoHeightDivM" style="max-height: 100%;">
               <?php foreach ($allRows as $song): ?>
-                <div id="song_<?php echo $song['id']; ?>" class="d-flex justify-content-between align-items-center rounded-4 bg-dark-subtle bg-opacity-10 my-2 <?php echo ($song['id'] == $row['id']) ? 'rounded-4 bg-body-tertiary border border-opacity-25 border-light' : ''; ?>">
-                  <a class="text-decoration-none music text-start w-100 text-white btn fw-bold border-0" href="play_all.php?album=<?php echo urlencode($song['album']); ?>&id=<?php echo $song['id']; ?>">
+                <?php
+                  // Use getID3 to analyze the music file
+                  $getID3 = new getID3();
+                  $fileInfo = $getID3->analyze($song['file']);
+                  getid3_lib::CopyTagsToComments($fileInfo);
+
+                  // Extract information
+                  $duration = !empty($fileInfo['playtime_string']) ? $fileInfo['playtime_string'] : 'Unknown';
+                ?>
+                <div id="songM_<?php echo $song['id']; ?>" class="d-flex justify-content-between align-items-center rounded-4 bg-dark-subtle bg-opacity-10 my-2 <?php echo ($song['id'] == $row['id']) ? 'rounded-4 bg-body-tertiary border border-opacity-25 border-light' : ''; ?>">
+                  <a class="text-decoration-none music text-start w-100 text-white btn fw-bold border-0" href="play_all.php?album=<?php echo urlencode($song['album']); ?>&id=<?php echo $song['id']; ?>" style="overflow-x: auto; white-space: nowrap;">
                     <?php echo $song['title']; ?><br>
-                    <small class="text-muted"><?php echo $song['artist']; ?> - <?php echo $song['album']; ?></small>
+                    <small class="text-muted"><?php echo $song['artist']; ?> - <?php echo $song['album']; ?></small><br>
+                    <small class="text-muted">Playtime : <?php echo $duration; ?></small>
                   </a>
                   <div class="dropdown dropdown-menu-end">
                     <button class="text-decoration-none text-white btn fw-bold border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-three-dots-vertical"></i></button>
@@ -679,6 +659,40 @@ if (isset($_POST['favorite'])) {
                       <li><button class="dropdown-item fw-medium" onclick="sharePageS('<?php echo $song['id']; ?>', '<?php echo $song['title']; ?>')"><i class="bi bi-share-fill"></i> share</button></li>
                       <li><a class="dropdown-item fw-medium" href="artist.php?id=<?php echo $song['userid']; ?>"><i class="bi bi-person-fill"></i> show artist</a></li>
                       <li><a class="dropdown-item fw-medium" href="album.php?album=<?php echo $song['album']; ?>"><i class="bi bi-disc-fill"></i> show album</a></li>
+                      <li><a class="dropdown-item fw-medium" href="<?php echo $song['file']; ?>" download><i class="bi bi-cloud-arrow-down-fill"></i> download</a></li>
+                    </ul>
+                  </div>
+                </div>
+              <?php endforeach; ?>
+              <br><br><br><br><br><br><br><br><br><br>
+            </div>
+          </div>
+          <div class="d-none d-md-block d-lg-block">
+            <h3 class="text-start fw-bold mb-3"><i class="bi bi-music-note-list"></i> all song lists</h3>
+            <div class="overflow-y-auto" id="autoHeightDiv" style="max-height: 100%;">
+              <?php foreach ($allRows as $song): ?>
+                <?php
+                  // Use getID3 to analyze the music file
+                  $getID3 = new getID3();
+                  $fileInfo = $getID3->analyze($song['file']);
+                  getid3_lib::CopyTagsToComments($fileInfo);
+
+                  // Extract information
+                  $duration = !empty($fileInfo['playtime_string']) ? $fileInfo['playtime_string'] : 'Unknown';
+                ?>
+                <div id="song_<?php echo $song['id']; ?>" class="d-flex justify-content-between align-items-center rounded-4 bg-dark-subtle bg-opacity-10 my-2 <?php echo ($song['id'] == $row['id']) ? 'rounded-4 bg-body-tertiary border border-opacity-25 border-light' : ''; ?>">
+                  <a class="text-decoration-none music text-start w-100 text-white btn fw-bold border-0" href="play_all.php?album=<?php echo urlencode($song['album']); ?>&id=<?php echo $song['id']; ?>" style="overflow-x: auto; white-space: nowrap;">
+                    <?php echo $song['title']; ?><br>
+                    <small class="text-muted"><?php echo $song['artist']; ?> - <?php echo $song['album']; ?></small><br>
+                    <small class="text-muted">Playtime : <?php echo $duration; ?></small>
+                  </a>
+                  <div class="dropdown dropdown-menu-end">
+                    <button class="text-decoration-none text-white btn fw-bold border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-three-dots-vertical"></i></button>
+                    <ul class="dropdown-menu rounded-4">
+                      <li><button class="dropdown-item fw-medium" onclick="sharePageS('<?php echo $song['id']; ?>', '<?php echo $song['title']; ?>')"><i class="bi bi-share-fill"></i> share</button></li>
+                      <li><a class="dropdown-item fw-medium" href="artist.php?id=<?php echo $song['userid']; ?>"><i class="bi bi-person-fill"></i> show artist</a></li>
+                      <li><a class="dropdown-item fw-medium" href="album.php?album=<?php echo $song['album']; ?>"><i class="bi bi-disc-fill"></i> show album</a></li>
+                      <li><a class="dropdown-item fw-medium" href="<?php echo $song['file']; ?>" download><i class="bi bi-cloud-arrow-down-fill"></i> download</a></li>
                     </ul>
                   </div>
                 </div>
