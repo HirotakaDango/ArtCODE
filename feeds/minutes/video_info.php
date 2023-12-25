e15f3e9b61aa6443ee9db9074c8fa6750a674f45
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-12-25 20:20:17 +0700

    Add files via upload

--- feeds/minutes/video_info.php
+++ feeds/minutes/video_info.php
@@ -1,8 +1,21 @@
+    <?php
+      // Use getID3 to analyze the music file
+      require_once('../music/getID3/getid3/getid3.php');
+      $getID3 = new getID3();
+      $fileInfo = $getID3->analyze($row['video']);
+      getid3_lib::CopyTagsToComments($fileInfo);
+
+      // Extract information
+      $duration = !empty($fileInfo['playtime_string']) ? $fileInfo['playtime_string'] : 'Unknown';
+    ?>
     <div class="col">
-      <div class="card shadow-sm h-100 position-relative rounded-4 border-0">
-        <a class="shadow position-relative btn p-0 ratio ratio-16x9 border-0" href="playing.php?id=<?php echo $row['id']; ?>">
-          <img class="w-100 object-fit-cover rounded-4 rounded-bottom-0" height="200" src="thumbnails/<?php echo $row['thumb']; ?>">
-        </a>
+      <div class="card shadow-sm h-100 rounded-4 border-0">
+        <div class="position-relative">
+          <a class="shadow btn p-0 ratio ratio-16x9 border-0" href="playing.php?id=<?php echo $row['id']; ?>">
+            <img class="w-100 object-fit-cover rounded-4 rounded-bottom-0" height="200" src="thumbnails/<?php echo $row['thumb']; ?>">
+          </a>
+          <p class="badge bg-dark opacity-75 position-absolute bottom-0 end-0 m-2"><?php echo $duration; ?></p>
+        </div>
         <div class="p-2 bg-body-tertiary rounded-bottom-4">
           <h5 class="card-text fw-bold text-shadow">
             <?php echo (!is_null($row['title']) && strlen($row['title']) > 15) ? substr($row['title'], 0, 15) . '...' : $row['title']; ?>
