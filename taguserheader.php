6459e73e03445a04c5e77b0e261cb7c1ed1e5088
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2024-01-05 00:49:23 +0700

    Add files via upload

--- taguserheader.php
+++ taguserheader.php
@@ -1,9 +1,4 @@
-    <ul class="me-2 ms-2 mt-2 nav  nav-fill justify-content-center">
-      <li class="nav-item"><a class="nav-link fs-big text-secondary fw-bold <?php if(basename($_SERVER['PHP_SELF']) == 'tags.php') echo 'border-bottom border-3' ?>" href="tags.php"><i class="bi bi-tags-fill"></i> Tags</a></li>
-      <li class="nav-item"><a class="nav-link fs-big text-secondary fw-bold <?php if(basename($_SERVER['PHP_SELF']) == 'users.php') echo 'border-bottom border-3' ?>" href="users.php"><i class="bi bi-people-fill"></i> Users</a></li>
-    </ul> 
-    <style>
-      .fs-big {
-        font-size: 18px;
-      } 
-    </style>
+    <div class="btn-group w-100 gap-2 my-3 container-fluid">
+      <a class="btn bg-body-tertiary p-4 fw-bold w-50 rounded-4 shadow <?php if(basename($_SERVER['PHP_SELF']) == 'tags.php') echo 'opacity-75' ?>" href="tags.php"><i class="bi bi-tags-fill"></i> Tags</a>
+      <a class="btn bg-body-tertiary p-4 fw-bold w-50 rounded-4 shadow <?php if(basename($_SERVER['PHP_SELF']) == 'users.php') echo 'opacity-75' ?>" href="users.php"><i class="bi bi-people-fill"></i> Users</a>
+    </div> 
