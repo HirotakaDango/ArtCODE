da3c4dc3a6e89f5f87ed160f8aca10e6e0c58511
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2024-06-24 02:32:35 +0700

    Add files via upload

--- favorites/image_card_favorites.php
+++ favorites/image_card_favorites.php
@@ -1,15 +1,36 @@
     <div class="w-100 px-1">
-      <div class="row row-cols-2 row-cols-sm-2 row-cols-md-5 g-1">
+      <div class="<?php include('../rows_columns/row-cols.php'); echo $rows_columns; ?>">
         <?php foreach ($favorite_images as $image): ?>
           <div class="col">
             <div class="position-relative">
               <a class="rounded ratio ratio-1x1" href="../image.php?artworkid=<?php echo $image['id']; ?>">
                 <img class="rounded shadow object-fit-cover lazy-load <?php echo ($image['type'] === 'nsfw') ? 'nsfw' : ''; ?>" data-src="../thumbnails/<?php echo $image['filename']; ?>" alt="<?php echo $image['title']; ?>">
               </a> 
+              <?php
+                $current_image_id = $image['id'];
+                
+                // Query to count main image from the images table
+                $stmt = $db->prepare("SELECT COUNT(*) as image_count FROM images WHERE id = :id");
+                $stmt->bindValue(':id', $current_image_id, PDO::PARAM_INT);
+                $stmt->execute();
+                $imageCountRow = $stmt->fetch(PDO::FETCH_ASSOC);
+                $imageCount = $imageCountRow ? $imageCountRow['image_count'] : 0;
+            
+                // Query to count associated images from the image_child table
+                $stmt = $db->prepare("SELECT COUNT(*) as child_image_count FROM image_child WHERE image_id = :image_id");
+                $stmt->bindValue(':image_id', $current_image_id, PDO::PARAM_INT);
+                $stmt->execute();
+                $childImageCountRow = $stmt->fetch(PDO::FETCH_ASSOC);
+                $childImageCount = $childImageCountRow ? $childImageCountRow['child_image_count'] : 0;
+            
+                // Total count of main images and associated images
+                $totalImagesCount = $imageCount + $childImageCount;
+              ?>
+              <?php include('../rows_columns/image_counts.php'); ?>
               <div class="position-absolute top-0 start-0">
                 <div class="dropdown">
-                  <button class="btn btn-sm btn-dark ms-1 mt-1 rounded-1 opacity-50" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="bi bi-three-dots-vertical"></i>
+                  <button class="btn border-0 p-1" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi bi-three-dots-vertical text-white link-body-emphasis fs-5" style="text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.4), 2px 2px 4px rgba(0, 0, 0, 0.3), 3px 3px 6px rgba(0, 0, 0, 0.2); text-stroke: 2;"></i>
                   </button>
                   <ul class="dropdown-menu">
                     <?php
---
+++ rows_columns/row-cols.php
@@ -0,0 +1,3 @@
+<?php
+
+$rows_columns = "row row-cols-2 row-cols-sm-2 row-cols-md-6 g-1";
