15fd9f0f04148f1e41979eff7982847d77898041
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2024-07-02 03:03:01 +0700

    Delete taguserheader.php

--- taguserheader.php
+++
@@ -1,4 +0,0 @@
-    <div class="btn-group w-100 gap-2 my-3 container-fluid">
-      <a class="btn bg-body-tertiary p-4 fw-bold w-50 rounded-4 shadow <?php if(basename($_SERVER['PHP_SELF']) == 'tags.php') echo 'opacity-75' ?>" href="tags.php"><i class="bi bi-tags-fill"></i> Tags</a>
-      <a class="btn bg-body-tertiary p-4 fw-bold w-50 rounded-4 shadow <?php if(basename($_SERVER['PHP_SELF']) == 'users.php') echo 'opacity-75' ?>" href="users.php"><i class="bi bi-people-fill"></i> Users</a>
-    </div> 
