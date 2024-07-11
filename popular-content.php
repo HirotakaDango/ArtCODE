8b1a2a906bcab0d8dc112169cb503af428815df7
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2024-07-12 01:02:26 +0700

    Delete popular-content.php

--- popular-content.php
+++
@@ -1,8 +0,0 @@
-<?php include 'contents/popular_contents/style.php'; ?>
-<?php include 'contents/popular_contents/button.php'; ?>
-
-<div class="collapse" id="collapsePopular">
-  <?php include 'contents/popular_contents/desktop.php'; ?>
-</div>
-
-<?php include 'contents/popular_contents/mobile.php'; ?>
