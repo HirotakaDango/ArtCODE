acb440fe2335b1891ce975357e393d2ab635cad1
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2024-07-12 01:30:32 +0700

    Add files via upload

--- feeds/scrolls/view_post.php
+++ feeds/scrolls/view_post.php
@@ -1,7 +1,7 @@
 <div class="modal fade" id="viewPost<?php echo $image['id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-fullscreen">
     <div class="modal-content border-0 rounded-0">
-      <div class="container-fluid px-0 py-2" style="max-width: 750px; margin-top: -17px;">
+      <div class="container-fluid px-0 py-2" style="max-width: 750px; margin-top: -16px;">
         <button type="button" class="btn border-0 text-start position-absolute start-0 top-0 p-2" data-bs-dismiss="modal">
           <i class="bi bi-chevron-left" style="-webkit-text-stroke: 2px;"></i>
         </button>
