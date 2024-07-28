858ee9d2bf8a6c2ce37c7c5e3477c0e363eb4ded
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2024-07-29 01:46:23 +0700

    Update connection.php

--- manga/connection.php
+++ manga/connection.php
@@ -1,7 +1,7 @@
 <?php
 // Base URL for the web server
 
-default web url destination
+// default web url destination
 $web = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'];
 
 // current web url destination
