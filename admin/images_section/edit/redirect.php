66a9ebe9d749c7cbe860229d93d60f094e053845
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2024-08-20 00:46:02 +0700

    Delete admin/images_section directory

--- admin/images_section/edit/redirect.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Determine the back URL or default to the current URL with a specified path
-$backUrl = isset($_GET['back']) ? $_GET['back'] : (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/admin/images_section/';
-
-// Redirect to the specified URL
-header("Location: " . $backUrl);
-exit();
-?>
