f4c3d0809e569c3857cc98044583f280203768c1
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2024-08-24 04:08:43 +0700

    Add files via upload

--- preview/keyword/index.php
+++ preview/keyword/index.php
@@ -43,6 +43,7 @@ $output = '';
 <html lang="en">
   <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
       <?php echo $output; ?>
     </title>
