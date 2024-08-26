6a543df96cda05c0d2bac1c35aa5e4406152c379
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2024-08-26 19:28:33 +0700

    Add files via upload

--- api_images/navbar.php
+++ api_images/navbar.php
@@ -2,7 +2,7 @@
       <div class="container-fluid gap-2 justify-content-end">
         <a class="navbar-brand me-auto fw-bold" href="redirect.php?back=<?php echo urlencode(isset($_GET['back']) ? $_GET['back'] : (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/index.php'); ?>">ArtCODE - API</a>
         <button id="themeToggle" class="btn px-0 border-0">
-          <i id="themeIcon" class="bi"></i>
+          <i id="themeIcon" class="bi bi-sun-fill"></i>
         </button>
       </div>
     </nav>
