fe97928a842c2e82912d478862bfa2e8735315d2
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2025-05-08 02:20:21 +0700

    Add files via upload

--- admin/images_section/similar_image_search/image_card_similar.php
+++ admin/images_section/similar_image_search/image_card_similar.php
@@ -4,7 +4,7 @@
               <div class="col">
                 <div class="position-relative">
                   <a class="rounded ratio ratio-1x1" href="/image.php?artworkid=<?php echo $image['id']; ?>">
-                    <img class="rounded shadow object-fit-cover lazy-load <?php echo ($image['type'] === 'nsfw') ? 'nsfw' : ''; ?>" data-src="/thumbnails/<?php echo $image['filename']; ?>" alt="<?php echo $image['title']; ?>">
+                    <img class="rounded rounded-bottom-0 shadow object-fit-cover lazy-load <?php echo ($image['type'] === 'nsfw') ? 'nsfw' : ''; ?>" data-src="/thumbnails/<?php echo $image['filename']; ?>" alt="<?php echo $image['title']; ?>">
                   </a> 
                   <span class="position-absolute top-0 end-0 badge bg-dark bg-opacity-75 m-2">
                     <?php echo number_format($image['similarity'] * 100, 1); ?>% similarity
@@ -30,7 +30,6 @@
                     $totalImagesCount = $imageCount + $childImageCount;
                   ?>
                   <?php include($_SERVER['DOCUMENT_ROOT'] . '/rows_columns/image_counts.php'); ?>
-                  <div class="position-absolute top-0 start-0">
                     <div class="dropdown">
                       <button class="btn border-0 p-1" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="bi bi-three-dots-vertical text-white link-body-emphasis fs-5" style="text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.4), 2px 2px 4px rgba(0, 0, 0, 0.3), 3px 3px 6px rgba(0, 0, 0, 0.2); text-stroke: 2;"></i>
