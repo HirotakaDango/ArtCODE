2d8ba5ecd123bdae260aed0ad96a2d47c484cda7
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2025-05-08 22:01:29 +0700

    Add files via upload

--- home/best_mobile/image_card_best.php
+++ home/best_mobile/image_card_best.php
@@ -1,118 +1,105 @@
-    <div class="w-100 px-1 my-2">
-      <h5 class="fw-bold px-1">Popular Artworks</h5>
-      <h6 class="fw-bold small px-1 mb-3">
-        These images are displayed based on their view counts from <?php $time = isset($_GET['time']) ? $_GET['time'] : 'day'; echo $time === 'alltime' ? 'all time' : "this $time"; ?>. The more views an image has, the higher its ranking in this list.
-      </h6>
-      <div class="<?php include('../rows_columns/row-cols.php'); echo $rows_columns; ?>">
-        <?php 
-        $j = 0; // Initialize the counter variable
-        while ($image = $result->fetchArray()): 
-        ?>
-          <div class="col position-relative">
-            <div class="position-relative">
-              <a class="rounded ratio ratio-1x1" href="../image.php?artworkid=<?php echo $image['id']; ?>">
-                <img class="rounded shadow object-fit-cover lazy-load <?php echo ($image['type'] === 'nsfw') ? 'nsfw' : ''; ?>" data-src="../thumbnails/<?php echo $image['filename']; ?>" alt="<?php echo $image['title']; ?>">
-              </a>
-              <?php
-                $current_image_id = $image['id'];
-                
-                // Query to count main image from the images table
-                $stmt = $db->prepare("SELECT COUNT(*) as image_count FROM images WHERE id = :id");
-                $stmt->bindValue(':id', $current_image_id, SQLITE3_INTEGER);
-                $imageCountQuery = $stmt->execute();
-                $imageCount = $imageCountQuery ? $imageCountQuery->fetchArray(SQLITE3_ASSOC)['image_count'] : 0;
-              
-                // Query to count associated images from the image_child table
-                $stmt = $db->prepare("SELECT COUNT(*) as child_image_count FROM image_child WHERE image_id = :image_id");
-                $stmt->bindValue(':image_id', $current_image_id, SQLITE3_INTEGER);
-                $childImageCountQuery = $stmt->execute();
-                $childImageCount = $childImageCountQuery ? $childImageCountQuery->fetchArray(SQLITE3_ASSOC)['child_image_count'] : 0;
-              
-                // Total count of main images and associated images
-                $totalImagesCount = $imageCount + $childImageCount;
+<div id="carouselWrapperManga" class="mb-3 position-relative" style="height: 115vh; width: 100%;">
+  <!-- Carousel Div -->
+  <div class="position-relative w-100 h-100 d-flex align-items-center justify-content-center">
+    <div class="container-fluid px-5">
+      <div id="imageCarouselManga" class="carousel slide" data-bs-ride="carousel">
+        <h5 class="fw-bold text-<?php include($_SERVER['DOCUMENT_ROOT'] . '/appearance/opposite.php'); ?>">Popular Manga</h5>
+        <h6 class="text-<?php include($_SERVER['DOCUMENT_ROOT'] . '/appearance/opposite.php'); ?> fw-bold small">
+          These manga are displayed based on their view counts from <?php $timeManga = isset($_GET['time']) ? $_GET['time'] : 'day'; echo $timeManga === 'alltime' ? 'all time' : "this $timeManga"; ?>. The more views a manga has, the higher its ranking in this list.
+        </h6>
+        <div class="carousel-inner">
+          <?php
+          $totalImagesManga = count($imagesManga);
+          $slidesCountManga = ceil($totalImagesManga / 6);
+          
+          for ($iManga = 0; $iManga < $slidesCountManga; $iManga++) :
+            $startIndexManga = $iManga * 6;
+            $endIndexManga = min($startIndexManga + 6, $totalImagesManga);
+          ?>
+            <div class="carousel-item <?php echo $iManga === 0 ? 'active' : ''; ?>" data-bs-interval="false">
+              <div class="position-relative">
+                <div class="row row-cols-6 g-1">
+                  <?php for ($jManga = $startIndexManga; $jManga < $endIndexManga; $jManga++) :
+                    $imageUManga = $imagesManga[$jManga];
+                    $image_idManga = $imageUManga['id'];
+                    $image_urlManga = $imageUManga['filename'];
+                    $image_titleManga = $imageUManga['title'];
+                    $image_episodeManga = $imageUManga['episode_name'];
+                    $artist_nameManga = $imageUManga['artist'];
+                    $user_idManga = $imageUManga['user_id'];
+                    $userPicManga = $imageUManga['pic'];
+                    $viewsManga = $imageUManga['views']; // Get the views count
+                    $current_image_idManga = isset($_GET['artworkid']) ? $_GET['artworkid'] : null;
+                    
+                    $artist_nameManga = substr($artist_nameManga, 0, 10);
+                    $image_episodeManga = mb_substr($image_episodeManga, 0, 15, 'UTF-8');
+                    
+                    // Optionally, you can add an ellipsis if the string was truncated
+                    if (mb_strlen($image_episodeManga, 'UTF-8') > 15) {
+                      $image_episodeManga .= '...';
+                    }
     
-                // Determine badge color
-                $badgeClass = '';
-                switch ($j) {
-                  case 0:
-                    $badgeClass = 'gold text-white shadow'; // Gold
-                    break;
-                  case 1:
-                    $badgeClass = 'silver text-white shadow'; // Silver
-                    break;
-                  case 2:
-                    $badgeClass = 'bronze text-white shadow'; // Bronze
-                    break;
-                  default:
-                    $badgeClass = 'nothing text-white shadow'; // Default color for others
-                }
-              ?>
-              <?php include('../rows_columns/image_counts.php'); ?>
-              <span class="position-absolute top-0 end-0 m-2 badge <?php echo $badgeClass; ?>" style="text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.4);">No. <?php echo $j + 1; ?></span>
-              <div class="position-absolute top-0 start-0">
-                <div class="dropdown">
-                  <button class="btn border-0 p-1" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="bi bi-three-dots-vertical text-white link-body-emphasis fs-5" style="text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.4), 2px 2px 4px rgba(0, 0, 0, 0.3), 3px 3px 6px rgba(0, 0, 0, 0.2);"></i>
-                  </button>
-                  <ul class="dropdown-menu">
-                    <?php
-                      $is_favorited = $db->querySingle("SELECT COUNT(*) FROM favorites WHERE email = '$email' AND image_id = {$image['id']}");
-                      if ($is_favorited) {
-                    ?>
-                      <form method="POST">
-                        <input type="hidden" name="image_id" value="<?php echo $image['id']; ?>">
-                        <li><button type="submit" class="dropdown-item fw-bold" name="unfavorite"><i class="bi bi-heart-fill"></i> <small>Unfavorite</small></button></li>
-                      </form>
-                    <?php } else { ?>
-                      <form method="POST">
-                        <input type="hidden" name="image_id" value="<?php echo $image['id']; ?>">
-                        <li><button type="submit" class="dropdown-item fw-bold" name="favorite"><i class="bi bi-heart"></i> <small>Favorite</small></button></li>
-                      </form>
-                    <?php } ?>
-                    <li><button class="dropdown-item fw-bold" data-bs-toggle="modal" data-bs-target="#shareImage<?php echo $image['id']; ?>"><i class="bi bi-share-fill"></i> <small>Share</small></button></li>
-                    <li><button class="dropdown-item fw-bold" data-bs-toggle="modal" data-bs-target="#infoImage_<?php echo $image['id']; ?>"><i class="bi bi-info-circle-fill"></i> <small>Info</small></button></li>
-                  </ul>
-                  <?php include('share_home.php'); ?>
-                  <?php include('../contents/card_image_3.php'); ?>
+                    // Determine badge color
+                    $badgeClassManga = '';
+                    switch ($jManga) {
+                      case 0:
+                        $badgeClassManga = 'gold text-white shadow'; // Gold
+                        break;
+                      case 1:
+                        $badgeClassManga = 'silver text-white shadow'; // Silver
+                        break;
+                      case 2:
+                        $badgeClassManga = 'bronze text-white shadow'; // Bronze (you may need to define .bg-brown in your CSS)
+                        break;
+                      default:
+                        $badgeClassManga = 'nothing text-white shadow'; // Default color for others
+                    }
+                  ?>
+                    <div class="col position-relative">
+                      <div class="position-relative">
+                        <a href="/manga/title.php?title=<?= urlencode($image_episodeManga); ?>&uid=<?= $user_idManga; ?>">
+                          <div class="ratio-cover">
+                            <img class="object-fit-cover rounded rounded-bottom-0" src="/thumbnails/<?php echo $image_urlManga; ?>" alt="<?php echo $image_episodeManga; ?>" style="object-fit: cover;">
+                          </div>
+                        </a>
+                        <!-- Badge for rank -->
+                        <span class="position-absolute top-0 start-0 m-2 badge <?php echo $badgeClassManga; ?>" style="text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.4);">No. <?php echo $jManga + 1; ?></span>
+                        <!-- Badge for views count -->
+                      </div>
+                      <div class="d-flex align-items-center p-2 bg-light rounded-bottom mx-0 text-dark">
+                        <img class="rounded-circle object-fit-cover border border-1" width="40" height="40" src="/<?php echo !empty($userPicManga) ? $userPicManga : 'icon/profile.svg'; ?>" alt="Profile Picture" style="margin-top: -2px;">
+                        <div class="ms-2">
+                          <div class="fw-bold text-truncate" style="max-width: 140px;"><?php echo $image_episodeManga; ?></div>
+                          <a class="fw-medium text-decoration-none text-dark link-body-emphasis small text-truncate" style="max-width: 140px;" href="#" type="button" data-bs-toggle="modal" data-bs-target="#userModalBestManga-<?php echo $user_idManga; ?>"><?php echo $artist_nameManga; ?></a>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="modal fade" id="userModalBestManga-<?php echo $user_idManga; ?>" tabindex="-1" aria-labelledby="userModalLabelBestManga" aria-hidden="true">
+                      <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content bg-transparent border-0">
+                          <div class="modal-body position-relative">
+                            <a class="position-absolute top-0 end-0 m-4" href="/artist.php?id=<?php echo urlencode($user_idManga); ?>" target="_blank">
+                              <i class="bi bi-box-arrow-up-right link-body-emphasis text-white" style="-webkit-text-stroke: 1px; text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.4), 2px 2px 4px rgba(0, 0, 0, 0.3), 3px 3px 6px rgba(0, 0, 0, 0.2); text-stroke: 2;"></i>
+                            </a>
+                            <iframe src="/rows_columns/user_preview.php?id=<?php echo urlencode($user_idManga); ?>" class="rounded-4 p-0 shadow" width="100%" height="300" style="border: none;"></iframe>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  <?php endfor; ?>
                 </div>
               </div>
             </div>
-          </div>
-          <?php 
-            $j++; // Increment the counter after each image
-          endwhile; 
-          ?>
+          <?php endfor; ?>
+        </div>
       </div>
     </div>
-    <style>
-      .gold {
-        background-color: gold;
-      }
-    
-      .silver {
-        background-color: silver;
-      }
-    
-      .bronze {
-        background-color: brown;
-      }
-      
-      .nothing {
-        background-color: lightgray;
-      }
- 
-       .ratio-cover {
-        position: relative;
-        width: 100%;
-        padding-bottom: 140%;
-      }
-
-      .ratio-cover img {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-      }
-    </style>
+    <!-- Custom Navigation Buttons -->
+    <button class="carousel-control-prev position-absolute top-50 start-0 translate-middle-y ms-4" type="button" data-bs-target="#imageCarouselManga" data-bs-slide="prev" style="background: rgba(0, 0, 0, 0.5); border: none; border-radius: 50%; width: 50px; height: 50px; display: flex; align-items: center; justify-content: center;">
+      <i class="bi bi-chevron-left text-white" style="font-size: 24px;"></i>
+    </button>
+    <button class="carousel-control-next position-absolute top-50 end-0 translate-middle-y me-4" type="button" data-bs-target="#imageCarouselManga" data-bs-slide="next" style="background: rgba(0, 0, 0, 0.5); border: none; border-radius: 50%; width: 50px; height: 50px; display: flex; align-items: center; justify-content: center;">
+      <i class="bi bi-chevron-right text-white" style="font-size: 24px;"></i>
+    </button>
+  </div>
+</div>
